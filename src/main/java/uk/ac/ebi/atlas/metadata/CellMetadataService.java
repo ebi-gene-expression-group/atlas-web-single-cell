00c2ca7a0fad0cc2c666710d1c15a3e4ea0d68e7
Monica Jianu <monica@ebi.ac.uk>
2019-04-11 16:43:32 +0100

    Removed unused methods as I am no longer emotionally attached to them. Cleaned up tests and removed redundant provider.

--- src/main/java/uk/ac/ebi/atlas/metadata/CellMetadataService.java
+++ src/main/java/uk/ac/ebi/atlas/metadata/CellMetadataService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -20,24 +19,6 @@ public class CellMetadataService {
 
     public CellMetadataService(CellMetadataDao cellMetadataDao) {
         this.cellMetadataDao = cellMetadataDao;
-    }
-
-    public Optional<String> getInferredCellType(String experimentAccession, String cellId) {
-        return cellMetadataDao.getMetadataValueForCellId(
-                experimentAccession, CHARACTERISTIC_INFERRED_CELL_TYPE,
-                cellId);
-    }
-
-    public Map<String, String> getFactors(String experimentAccession, String cellId) {
-        List<SingleCellAnalyticsSchemaField> factorFieldNames =
-                cellMetadataDao.getFactorFieldNames(experimentAccession, cellId);
-
-        return cellMetadataDao.getQueryResultForMultiValueFields(
-                experimentAccession, Optional.of(cellId), factorFieldNames)
-                .entrySet().stream()
-                .collect(toMap(
-                        entry -> SingleCellAnalyticsCollectionProxy.metadataFieldNameToDisplayName(entry.getKey()),
-                        entry -> entry.getValue().stream().map(Object::toString).collect(Collectors.joining(","))));
     }
 
     public Map<String, String> getMetadata(String experimentAccession, String cellId) {

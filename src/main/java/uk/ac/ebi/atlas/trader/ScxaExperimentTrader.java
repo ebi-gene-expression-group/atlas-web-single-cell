8344029f5f842e1d9a2e80a83d9bcd88028ff79b
amunozpomer <amunoz@ebi.ac.uk>
2019-04-16 19:23:16 +0100

    Revert "Add Eclipse annotations to reduce boilerplate annotations"
    This reverts commit c5932843

--- src/main/java/uk/ac/ebi/atlas/trader/ScxaExperimentTrader.java
+++ src/main/java/uk/ac/ebi/atlas/trader/ScxaExperimentTrader.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.atlas.trader;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.experimentimport.ExperimentDto;
 import uk.ac.ebi.atlas.experimentimport.ScxaExperimentDao;
@@ -9,7 +8,6 @@ import uk.ac.ebi.atlas.model.experiment.singlecell.SingleCellBaselineExperiment;
 import uk.ac.ebi.atlas.trader.factory.SingleCellBaselineExperimentFactory;
 
 @Component
-@NonNullByDefault
 public class ScxaExperimentTrader extends ExperimentTrader {
     private final SingleCellBaselineExperimentFactory experimentFactory;
 

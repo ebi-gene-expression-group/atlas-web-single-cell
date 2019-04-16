4f2305616bdbda43dbd143f7febbd7ad1917dff9
amunozpomer <amunoz@ebi.ac.uk>
2019-04-16 19:23:22 +0100

    Revert "DRY out ExperimentTrader"
    This reverts commit 070875d1

--- src/main/java/uk/ac/ebi/atlas/trader/ScxaExperimentTrader.java
+++ src/main/java/uk/ac/ebi/atlas/trader/ScxaExperimentTrader.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.experimentimport.ExperimentDto;
 import uk.ac.ebi.atlas.experimentimport.ScxaExperimentDao;
 import uk.ac.ebi.atlas.experimentimport.idf.IdfParser;
+import uk.ac.ebi.atlas.experimentimport.idf.IdfParserOutput;
+import uk.ac.ebi.atlas.model.experiment.ExperimentDesign;
 import uk.ac.ebi.atlas.model.experiment.singlecell.SingleCellBaselineExperiment;
 import uk.ac.ebi.atlas.trader.factory.SingleCellBaselineExperimentFactory;
 
@@ -20,10 +22,11 @@ public class ScxaExperimentTrader extends ExperimentTrader {
     }
 
     @Override
-    protected SingleCellBaselineExperiment buildExperiment(ExperimentDto experimentDto) {
-        return experimentFactory.create(
-                experimentDto,
-                experimentDesignParser.parse(experimentDto.getExperimentAccession()),
-                idfParser.parse(experimentDto.getExperimentAccession()));
+    public SingleCellBaselineExperiment getExperiment(String experimentAccession, String accessKey) {
+        ExperimentDesign experimentDesign = experimentDesignParser.parse(experimentAccession);
+        IdfParserOutput idfParserOutput = idfParser.parse(experimentAccession);
+        ExperimentDto experimentDto = experimentDao.findExperiment(experimentAccession, accessKey);
+
+        return experimentFactory.create(experimentDto, experimentDesign, idfParserOutput);
     }
 }

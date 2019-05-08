26a33c3fdbd48e977317c69d04c2c5cba0adb73b
amunozpomer <amunoz@ebi.ac.uk>
2019-05-08 19:56:12 +0100

    Build dev bundles

--- src/main/javascript/bundles/homepage-species-summary-panel/index.js
+++ src/main/javascript/bundles/homepage-species-summary-panel/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import SpeciesSummaryPanel from 'species-summary-panel'
-import withFetchLoader from 'atlas-react-fetch-loader'
+import { withFetchLoader } from 'atlas-react-fetch-loader'
 
 const FetchLoadSpeciesSummaryPanel = withFetchLoader(SpeciesSummaryPanel)
 

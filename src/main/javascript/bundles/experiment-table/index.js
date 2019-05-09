30df122b149166433476ffdd5e8841c4208fa247
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-09 17:03:12 +0100

    Wrap experiment table with fetch loader

--- src/main/javascript/bundles/experiment-table/index.js
+++ src/main/javascript/bundles/experiment-table/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import ExperimentTable from 'atlas-experiment-table'
+import withFetchLoader from 'atlas-react-fetch-loader'
+
+const FetchLoadExperimentTable = withFetchLoader(ExperimentTable)
 
 const render = (options, target) => {
-  ReactDOM.render(<ExperimentTable {...options} />, document.getElementById(target))
+  ReactDOM.render(
+  <FetchLoadExperimentTable
+  {...options}
+  />,
+  document.getElementById(target))
 }
 
 export { render }

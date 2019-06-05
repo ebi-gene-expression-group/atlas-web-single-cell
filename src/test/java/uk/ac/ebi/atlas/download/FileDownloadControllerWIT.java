58e304bb9e7aa4d56fc4607e17575f2098fc1434
lingyun1010 <zhaolingyun1010@gmail.com>
2019-06-05 15:52:22 +0100

    Add a test for check file controller

--- src/test/java/uk/ac/ebi/atlas/download/FileDownloadControllerWIT.java
+++ src/test/java/uk/ac/ebi/atlas/download/FileDownloadControllerWIT.java
@@ -27,14 +27,19 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.isA;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static uk.ac.ebi.atlas.testutils.RandomDataTestUtils.generateRandomExperimentAccession;
@@ -52,6 +57,7 @@ class FileDownloadControllerWIT {
     private static final String FILE_DOWNLOAD_URL = "/experiment/{experimentAccession}/download";
     private static final String ARCHIVE_DOWNLOAD_URL = "/experiment/{experimentAccession}/download/zip";
     private static final String ARCHIVE_DOWNLOAD_LIST_URL = "/experiments/download/zip";
+    private static final String ARCHIVE_CHECK_LIST_URL = "/experiments/check/zip";
 
     @Inject
     private DataSource dataSource;
@@ -234,5 +240,18 @@ class FileDownloadControllerWIT {
                 .map(Path::getFileName)
                 .map(Path::toString)
                 .collect(toImmutableList());
+    }
+
+    @Test
+    void checkArchiveForValidExperimentAccession() throws Exception {
+        this.mockMvc.perform(get(ARCHIVE_CHECK_LIST_URL)
+                .param("accession", EXPERIMENT_ACCESSION_LIST.get(0))
+                .param("accession", EXPERIMENT_ACCESSION_LIST.get(1)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.invalidFiles", isA(Map.class)))
+                .andExpect(jsonPath("$.invalidFiles", hasKey(EXPERIMENT_ACCESSION_LIST.get(0))))
+                .andExpect(jsonPath("$.invalidFiles", hasKey(EXPERIMENT_ACCESSION_LIST.get(1))));
+
     }
 }

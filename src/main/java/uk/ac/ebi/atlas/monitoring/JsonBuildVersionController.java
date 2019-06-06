cd13b03a585c1baa63613208d302b3f99e54bfb9
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2019-06-06 11:33:25 +0100

    Bugfix #166430782 – Show build info in `json/build` endpoint (#9)
    * Add build info properties for Gradle
    
    * Refactor and simplify monitoring endpoints

--- src/main/java/uk/ac/ebi/atlas/monitoring/JsonBuildVersionController.java
+++ src/main/java/uk/ac/ebi/atlas/monitoring/JsonBuildVersionController.java
@@ -9,41 +9,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 
-import javax.inject.Inject;
-
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
 @PropertySource("classpath:configuration.properties")
 public class JsonBuildVersionController extends JsonExceptionHandlingController {
+    private final ImmutableMap<String, String> buildVersion;
 
-    private String buildNumber;
-    private String buildBranch;
-    private String buildCommitId;
-    private String tomcatHostname;
-
-    @Inject
-    public JsonBuildVersionController(@Value("${buildNumber}") String buildNumber,
-                                      @Value("${buildBranch}") String buildBranch,
-                                      @Value("${buildCommitId}") String buildCommitId,
-                                      @Value("${tomcatHostname}") String tomcatHostname) {
-        this.buildNumber = buildNumber;
-        this.buildBranch = buildBranch;
-        this.buildCommitId = buildCommitId;
-        this.tomcatHostname = tomcatHostname;
+    public JsonBuildVersionController(@Value("${build.number}") String buildNumber,
+                                      @Value("${build.branch}") String buildBranch,
+                                      @Value("${build.commitId}") String buildCommitId,
+                                      @Value("${build.tomcatHostname}") String tomcatHostname) {
+        buildVersion = ImmutableMap.of(
+                "bambooBuildVersion", buildNumber,
+                "gitBranch", buildBranch,
+                "gitCommitID", buildCommitId,
+                "tomcatHostname", tomcatHostname);
     }
 
-    @RequestMapping(
-            value = "/json/build",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
-    )
+    @RequestMapping(value = "/json/build",
+                    method = RequestMethod.GET,
+                    produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getBuildInfo() {
-        return GSON.toJson(
-                ImmutableMap.of(
-                        "bambooBuildVersion", buildNumber,
-                        "gitBranch", buildBranch,
-                        "gitCommitID", buildCommitId,
-                        "tomcatHostname", tomcatHostname));
+        return GSON.toJson(buildVersion);
     }
 }

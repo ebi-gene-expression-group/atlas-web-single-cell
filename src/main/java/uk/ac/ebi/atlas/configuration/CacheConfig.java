1f3de2661cc6a6ed2d45b9ad838732330be00318
amunozpomer <amunoz@ebi.ac.uk>
2019-08-01 10:58:04 +0100

    Disable caching for fallback environment

--- src/main/java/uk/ac/ebi/atlas/configuration/CacheConfig.java
+++ src/main/java/uk/ac/ebi/atlas/configuration/CacheConfig.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableCaching
+//@EnableCaching
 @Configuration
 public class CacheConfig {
     @Bean

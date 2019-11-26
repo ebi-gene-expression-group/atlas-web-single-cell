ff7acc39a6150eabdf9758d20be426199f10ec8d
Haider Iqbal <haideri@ebi.ac.uk>
2019-11-26 11:50:03 +0000

    - Refactor variable to camel case

--- src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsListController.java
+++ src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsListController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
@@ -19,9 +18,8 @@ public class ExperimentsListController {
 
     //Used by experiments table page
     @GetMapping(value = "/json/experiments",
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getExperimentsList(@RequestParam(defaultValue = "") String organism_part) {
-        return isNullOrEmpty(organism_part) ? GSON.toJson(experimentInfoListService.getExperimentsJson()) :
-                GSON.toJson(experimentInfoListService.getExperimentsJson(CHARACTERISTIC_NAME, organism_part));
+                produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getExperimentsList(@RequestParam(defaultValue = "") String organismPart) {
+        return GSON.toJson(experimentInfoListService.getExperimentsJson(CHARACTERISTIC_NAME, organismPart));
     }
 }

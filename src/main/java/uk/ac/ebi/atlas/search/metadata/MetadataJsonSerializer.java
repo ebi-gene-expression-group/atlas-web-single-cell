c476f61df89b3fca098c93722de97ea8bd8b6e82
lingyun1010 <zhaolingyun1010@gmail.com>
2019-11-27 16:43:30 +0000

    Cluster cell type expressions and marker genes by
    species

--- src/main/java/uk/ac/ebi/atlas/search/metadata/MetadataJsonSerializer.java
+++ src/main/java/uk/ac/ebi/atlas/search/metadata/MetadataJsonSerializer.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.atlas.experimentpage.markergenes.MarkerGenesDao;
 import uk.ac.ebi.atlas.experimentpage.tsneplot.TSnePlotService;
 import uk.ac.ebi.atlas.experimentpage.tsneplot.TSnePlotSettingsService;
+import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.groupingBy;
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
@@ -21,15 +23,18 @@ public class MetadataJsonSerializer {
     private MarkerGenesDao markerGenesDao;
     private final TSnePlotSettingsService tsnePlotSettingsService;
     private MetadataSearchDao metadataSearchDao;
+    private ExperimentTrader experimentTrader;
 
     public MetadataJsonSerializer(TSnePlotService tSnePlotService,
                                   MarkerGenesDao markerGenesDao,
                                   MetadataSearchDao metadataSearchDao,
-                                  TSnePlotSettingsService tsnePlotSettingsService) {
+                                  TSnePlotSettingsService tsnePlotSettingsService,
+                                  ExperimentTrader experimentTrader) {
         this.tSnePlotService = tSnePlotService;
         this.markerGenesDao = markerGenesDao;
         this.tsnePlotSettingsService = tsnePlotSettingsService;
         this.metadataSearchDao = metadataSearchDao;
+        this.experimentTrader = experimentTrader;
     }
 
     @Cacheable(cacheNames = "jsonCellTypeMetadata", key = "{#characteristicName, #characteristicValue}")
@@ -38,68 +43,78 @@ public class MetadataJsonSerializer {
         //cell ids
         var cellIdsByExperimentAccession = fetchCellTypeMetadata(characteristicName, characteristicValue);
 
-        //marker genes ids
-        var markerGenesByExperimentAccession = cellIdsByExperimentAccession.keySet()
+        //experiment accessions by species
+        var experimentAccessionsBySpecies = cellIdsByExperimentAccession.keySet()
+                .stream()
+                .collect(groupingBy(experimentAccession ->
+                        experimentTrader.getExperiment(experimentAccession, accessKey).getSpecies().getName()));
+
+        //marker genes by species
+        var markerGenesBySpecies = experimentAccessionsBySpecies
+                .entrySet()
                 .stream()
                 .collect(toMap(
-                        experimentAccession -> experimentAccession,
-                        experimentAccession ->  markerGenesDao.getMarkerGenesWithAveragesPerCluster(experimentAccession,
-                                tsnePlotSettingsService.getExpectedClusters(experimentAccession).orElse(10))
-                                .stream()
-                                .map(makergene -> makergene.geneId())
-                                .distinct()
-                                .collect(Collectors.toList()))
+                        Map.Entry::getKey,
+                        entry ->  entry.getValue().stream()
+                                .flatMap(
+                                        accession -> markerGenesDao.getMarkerGenesWithAveragesPerCluster(accession,
+                                                tsnePlotSettingsService.getExpectedClusters(accession).orElse(10))
+                                                .stream()
+                                                .map(makergene -> makergene.geneId())
 
+                                ).collect(Collectors.toList())
+                        )
                 );
-
-        var allMarkerGenes = markerGenesByExperimentAccession.values()
-                .stream().flatMap(x -> x.stream())
-                .distinct()
-                .collect(Collectors.toList());
 
         var expressionByExpressionWithCellType = new HashMap<>();
 
-        for (var experimentAccession : cellIdsByExperimentAccession.keySet()) {
-            var cellIdsFromCellTypeQuery = cellIdsByExperimentAccession.get(experimentAccession).keySet();
-            var perpelexity = tsnePlotSettingsService.getAvailablePerplexities(experimentAccession).get(0);
+        for (var species : experimentAccessionsBySpecies.keySet()) {
+            var expressionBySpecies = new HashMap<>();
 
-            var expressionByMarkerGene = new HashMap<>();
+            for (var experimentAccession : cellIdsByExperimentAccession.keySet()) {
+                var cellIdsFromCellTypeQuery = cellIdsByExperimentAccession.get(experimentAccession).keySet();
+                var perpelexity = tsnePlotSettingsService.getAvailablePerplexities(experimentAccession).get(0);
 
-            for (var markerGene : allMarkerGenes) {
-                var expressionByCellType = new HashMap<String, ArrayList<Double>>();
-                var pointsWithExpression =
-                        tSnePlotService.fetchTSnePlotWithExpression(experimentAccession, perpelexity, markerGene);
+                var expressionByMarkerGene = new HashMap<>();
 
-                for (var cellExpression : pointsWithExpression) {
-                    var cellId = cellExpression.name();
-                    if (cellIdsFromCellTypeQuery.contains(cellId)) {
-                        var cellType = cellIdsByExperimentAccession.get(experimentAccession).get(cellId);
-                        var expressionAddon = cellExpression.expressionLevel();
-                        var expressionList = expressionByCellType.get(cellType);
-                        if (expressionList == null) {
-                            var listOfExpressions = new ArrayList<Double>();
-                            listOfExpressions.add(expressionAddon.get());
-                            expressionByCellType.put(cellType, listOfExpressions);
-                        } else if (expressionAddon.get() > 0.0) {
-                            expressionList.add(expressionAddon.get());
+                for (var markerGene : markerGenesBySpecies.get(species)) {
+                    var expressionByCellType = new HashMap<String, ArrayList<Double>>();
+                    var pointsWithExpression =
+                            tSnePlotService.fetchTSnePlotWithExpression(experimentAccession, perpelexity, markerGene);
+
+                    for (var cellExpression : pointsWithExpression) {
+                        var cellId = cellExpression.name();
+                        if (cellIdsFromCellTypeQuery.contains(cellId)) {
+                            var cellType = cellIdsByExperimentAccession.get(experimentAccession).get(cellId);
+                            var expressionAddon = cellExpression.expressionLevel();
+                            var expressionList = expressionByCellType.get(cellType);
+                            if (expressionList == null) {
+                                var listOfExpressions = new ArrayList<Double>();
+                                listOfExpressions.add(expressionAddon.get());
+                                expressionByCellType.put(cellType, listOfExpressions);
+                            } else if (expressionAddon.get() > 0.0) {
+                                expressionList.add(expressionAddon.get());
+                            }
                         }
                     }
+                    expressionByMarkerGene.put(markerGene,
+                            expressionByCellType
+                                    .entrySet()
+                                    .stream()
+                                    .collect(toMap(Map.Entry::getKey,
+                                            entry -> entry.getValue().stream()
+                                                    .mapToDouble(expression -> expression)
+                                                    .average()
+                                                    .orElse(0.0)
+                                    ))
+                    );
                 }
-                expressionByMarkerGene.put(markerGene,
-                        expressionByCellType
-                                .entrySet()
-                                .stream()
-                                .collect(toMap(Map.Entry::getKey,
-                                        entry -> entry.getValue().stream()
-                                                .mapToDouble(expression -> expression)
-                                                .average()
-                                                .orElse(0.0)
-                                ))
-                );
-            }
-            expressionByExpressionWithCellType.put(experimentAccession, expressionByMarkerGene);
+                expressionBySpecies.put(experimentAccession, expressionByMarkerGene);
 
+            }
+            expressionByExpressionWithCellType.put(species, expressionBySpecies);
         }
+
 
         return GSON.toJson(
                 ImmutableMap.of(

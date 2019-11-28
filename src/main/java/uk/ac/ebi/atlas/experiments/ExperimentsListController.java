cc557b071a12323382bc162c6548f815c05c080c
Haider Iqbal <haideri@ebi.ac.uk>
2019-11-28 11:19:56 +0000

    Add param to experiments json endpoint (#65)
    * - Add param to experiments json endpoint
    
    * - Refactor variable to camel case
    
    * - Add tests for ExperimentsListController

--- src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsListController.java
+++ src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsListController.java
@@ -2,12 +2,14 @@ package uk.ac.ebi.atlas.experiments;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
 public class ExperimentsListController {
+    private final static String CHARACTERISTIC_NAME = "organism_part";
     private final ExperimentInfoListService experimentInfoListService;
 
     public ExperimentsListController(ExperimentInfoListService experimentInfoListService) {
@@ -17,7 +19,7 @@ public class ExperimentsListController {
     //Used by experiments table page
     @GetMapping(value = "/json/experiments",
                 produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getExperimentsList() {
-        return GSON.toJson(experimentInfoListService.getExperimentsJson());
+    public String getExperimentsList(@RequestParam(defaultValue = "") String organismPart) {
+        return GSON.toJson(experimentInfoListService.getExperimentsJson(CHARACTERISTIC_NAME, organismPart));
     }
 }

e15d7c8841aedfc635a0ac92aaaeb2542b8826a6
amunozpomer <amunoz@ebi.ac.uk>
2019-12-12 17:28:48 +0000

    Force-create new context before entering tests

--- src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
+++ src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
@@ -1,16 +1,14 @@
 package uk.ac.ebi.atlas.home;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,15 +19,16 @@ import uk.ac.ebi.atlas.configuration.TestConfig;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = BEFORE_CLASS)
 @WebAppConfiguration
 @ContextConfiguration(classes = TestConfig.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JsonExperimentsSummaryControllerWIT {
     @Inject
     private DataSource dataSource;
@@ -39,23 +38,20 @@ public class JsonExperimentsSummaryControllerWIT {
     private MockMvc mockMvc;
 
     private static final String ENDPOINT_URL = "/json/experiments-summary";
-    @BeforeAll
-    void populateDatabaseTables() {
+
+    @BeforeEach
+    void setUp() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScripts(new ClassPathResource("fixtures/experiment-fixture.sql"));
         populator.execute(dataSource);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
-    @AfterAll
+    @AfterEach
     void cleanDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScripts(new ClassPathResource("fixtures/experiment-delete.sql"));
         populator.execute(dataSource);
-    }
-
-    @BeforeEach
-    void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test
--- src/test/resources/fixtures/experiment-fixture.sql
+++ src/test/resources/fixtures/experiment-fixture.sql
@@ -1,4 +1,4 @@
--- SELECT * FROM scxa_experiment
+-- SELECT * FROM experiment
 
 INSERT INTO experiment (accession, type, species, access_key, private, load_date, last_update, pubmed_ids, dois) VALUES ('E-MTAB-5061', 'SINGLE_CELL_RNASEQ_MRNA_BASELINE', 'Homo sapiens', '606a78ae-d6d5-4b6e-8b8a-787ca117108c', FALSE, '2018-10-11 12:34:56.789012', '2018-10-11 12:34:56.789012', '27667667', '10.1016/j.cmet.2016.08.020');
 INSERT INTO experiment (accession, type, species, access_key, private, load_date, last_update, pubmed_ids, dois) VALUES ('E-GEOD-99058', 'SINGLE_CELL_RNASEQ_MRNA_BASELINE', 'Mus musculus', 'a84c5ba3-9ea6-49d1-bc0f-5690cf679de0', FALSE, '2018-10-11 12:34:56.789012', '2018-10-11 12:34:56.789012', '29443965', '10.1038/nature25739');

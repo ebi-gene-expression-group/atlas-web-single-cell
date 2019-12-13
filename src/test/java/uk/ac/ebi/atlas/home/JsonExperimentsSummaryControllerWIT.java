6f0f9032f262ac9dd0a4da4f2376785ffc0031ae
amunozpomer <amunoz@ebi.ac.uk>
2019-12-13 20:59:59 +0000

    Update tests to match new fixtures and test datasets

--- src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
+++ src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
@@ -1,14 +1,16 @@
 package uk.ac.ebi.atlas.home;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,16 +21,15 @@ import uk.ac.ebi.atlas.configuration.TestConfig;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@DirtiesContext(classMode = BEFORE_CLASS)
 @WebAppConfiguration
 @ContextConfiguration(classes = TestConfig.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JsonExperimentsSummaryControllerWIT {
     @Inject
     private DataSource dataSource;
@@ -38,20 +39,23 @@ public class JsonExperimentsSummaryControllerWIT {
     private MockMvc mockMvc;
 
     private static final String ENDPOINT_URL = "/json/experiments-summary";
-
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    void populateDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScripts(new ClassPathResource("fixtures/experiment-fixture.sql"));
         populator.execute(dataSource);
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
-    @AfterEach
+    @AfterAll
     void cleanDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScripts(new ClassPathResource("fixtures/experiment-delete.sql"));
         populator.execute(dataSource);
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test

0493f83d7ce61d75ad4a4a413a770cbe4d199923
amunozpomer <amunoz@ebi.ac.uk>
2020-03-05 20:15:48 +0000

    The controller needs not be concerned about query params, the new table is fully routed

--- src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsController.java
+++ src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsController.java
@@ -3,15 +3,13 @@ package uk.ac.ebi.atlas.experiments;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 
 @Controller
 public class ExperimentsController extends HtmlExceptionHandlingController {
     @GetMapping(value = "/experiments", produces = "text/html;charset=UTF-8")
     public String
-    getExperimentsListParameters(@RequestParam(value = "species", required = false) String species, Model model) {
-        model.addAttribute("species", species);
+    getExperimentsListParameters(Model model) {
         return "experiments";
     }
 }

2ae6d88193f97c4991032f24f6b312e84decd0eb
amunozpomer <amunoz@ebi.ac.uk>
2020-03-09 17:07:00 +0000

    Fix download icons (#171705163)

--- src/main/javascript/bundles/experiment-page/src/supplementary-information/LinksToResources.js
+++ src/main/javascript/bundles/experiment-page/src/supplementary-information/LinksToResources.js
@@ -2,15 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import URI from 'urijs'
 
-import iconAe from './icons/ae-logo-64.png'
-import iconGseaReactome from './icons/gsea_reactome-icon.png'
-import iconGseaInterpro from './icons/gsea_interpro-icon.png'
-import iconGseaGo from './icons/gsea_go-icon.png'
-import iconMa from './icons/ma-plot-icon.png'
-import iconExperimentDesign from './icons/experiment_design_icon.png'
-import iconTsv from './icons/download_blue_small.png'
-import iconRdata from './icons/r-button.png'
-
 const LinksToResources = ({data, atlasUrl}) => {
   const links =  data.map((service, index) =>
     <div  key={index} style={{paddingBottom: `10px`}}>
@@ -34,14 +25,14 @@ const LinksToResources = ({data, atlasUrl}) => {
 
 const Icon = ({type}) => {
   const iconSrcMap = {
-    'icon-ae': iconAe,
-    'icon-gsea-reactome': iconGseaReactome,
-    'icon-gsea-interpro': iconGseaInterpro,
-    'icon-gsea-go': iconGseaGo,
-    'icon-ma': iconMa,
-    'icon-experiment-design': iconExperimentDesign,
-    'icon-tsv': iconTsv,
-    'icon-Rdata': iconRdata
+    'icon-ae': require(`./icons/ae-logo-64.png`),
+    'icon-gsea-reactome': require(`./icons/gsea_reactome-icon.png`),
+    'icon-gsea-interpro': require(`./icons/gsea_interpro-icon.png`),
+    'icon-gsea-go': require(`./icons/gsea_go-icon.png`),
+    'icon-ma': require(`./icons/ma-plot-icon.png`),
+    'icon-experiment-design': require(`./icons/experiment_design_icon.png`),
+    'icon-tsv': require(`./icons/download_blue_small.png`),
+    'icon-Rdata': require(`./icons/r-button.png`)
   }
 
   return (

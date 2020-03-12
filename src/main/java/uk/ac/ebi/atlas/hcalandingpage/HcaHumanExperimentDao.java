7de1d3cea85672715d034c02605651376038836e
Haider Iqbal <haideri@ebi.ac.uk>
2020-03-12 12:43:21 +0000

    New endpoint for organism_part related experiments (#80)
    * - Add new endpoint for organism_part related experiments
    - Add new classes for to serve new endpoint
    - Add corresponding test classes
    
    * - Add normalize clause to solr query
    - Add new test case for ontology Id
    
    * - Add filter to get only human experiments
    - Rename method to reflect functionality
    
    * - Add new controller for human experiments endpoint
    - Add related test cases
    - Refactor code
    
    * - Remove ScExperimentTrader move logic to service class
    - Refactor code accordingly
    
    * - Move classes to hca package
    - Refactor code

---
+++ src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaHumanExperimentDao.java
@@ -0,0 +1,57 @@
+package uk.ac.ebi.atlas.hcalandingpage;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
+import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy;
+import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.CHARACTERISTIC_NAME;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.CHARACTERISTIC_VALUE;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.EXPERIMENT_ACCESSION;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION_ANCESTORS_LABELS;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION_ANCESTORS_URIS;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION_LABEL;
+
+@Component
+public class HcaHumanExperimentDao {
+    private final SingleCellAnalyticsCollectionProxy singleCellAnalyticsCollectionProxy;
+
+    public HcaHumanExperimentDao(SolrCloudCollectionProxyFactory solrCloudCollectionProxyFactory) {
+        this.singleCellAnalyticsCollectionProxy = solrCloudCollectionProxyFactory.create(SingleCellAnalyticsCollectionProxy.class);
+    }
+
+    public ImmutableSet<String> fetchExperimentAccessions(String characteristicName, String characteristicValue) {
+        var queryBuilder = new SolrQueryBuilder<SingleCellAnalyticsCollectionProxy>();
+        /* Reason for checking blank here is in case characteristicValue is empty our solrQueryBuilder
+         * builds query like characteristic_value:("\*") which is different from characteristic_value:*
+         * which we need. So to handle that situation if characteristicValue is empty we build query
+         * characteristic_name:("organism_part") as we need all the experiments in case of empty characteristicValue
+         * */
+        queryBuilder
+                .setNormalize(false)
+                .addQueryFieldByTerm(CHARACTERISTIC_NAME, characteristicName)
+                .setFieldList(EXPERIMENT_ACCESSION);
+        if (isNotBlank(characteristicValue)) {
+            queryBuilder
+                    .addQueryFieldByTerm(ImmutableMap.of(
+                            CHARACTERISTIC_VALUE, ImmutableList.of("*".concat(characteristicValue)),
+                            ONTOLOGY_ANNOTATION, ImmutableList.of("*".concat(characteristicValue)),
+                            ONTOLOGY_ANNOTATION_ANCESTORS_LABELS, ImmutableList.of("*".concat(characteristicValue)),
+                            ONTOLOGY_ANNOTATION_ANCESTORS_URIS, ImmutableList.of("*".concat(characteristicValue)),
+                            ONTOLOGY_ANNOTATION_LABEL, ImmutableList.of("*".concat(characteristicValue))
+                    ));
+        }
+
+        var results = this.singleCellAnalyticsCollectionProxy.query(queryBuilder).getResults();
+        return results
+                .stream()
+                .map(solrDocument -> (String) solrDocument.getFieldValue(EXPERIMENT_ACCESSION.name()))
+                .collect(toImmutableSet());
+    }
+}

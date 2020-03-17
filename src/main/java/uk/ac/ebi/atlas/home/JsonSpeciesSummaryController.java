63fa8bff51860b262ee0dfcb2ddcd2a238312be3
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-03-17 16:40:07 +0000

    Bugfix #171702527 – Web VM host names are leaked to public-facing links (#90)
    * Production bundles
    
    * Do not cache the JSON response of species summaries

--- src/main/java/uk/ac/ebi/atlas/home/JsonSpeciesSummaryController.java
+++ src/main/java/uk/ac/ebi/atlas/home/JsonSpeciesSummaryController.java
@@ -25,7 +25,6 @@ public class JsonSpeciesSummaryController extends JsonExceptionHandlingControlle
         this.speciesSummarySerializer = speciesSummarySerializer;
     }
 
-    @Cacheable("speciesSummary")
     @GetMapping(value = "/json/species-summary",
                 produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getSpeciesSummaryGroupedByKingdom() {

2727bfd1b0d7a698be6b6fa51bf849b6f88bd908
amunozpomer <amunoz@ebi.ac.uk>
2020-03-23 22:03:16 +0000

    Update to latest dependencies and generate development bundles

--- src/main/webapp/resources/js-bundles/geneSearchForm.bundle.js
+++ src/main/webapp/resources/js-bundles/geneSearchForm.bundle.js
@@ -1,1 +1,16 @@
-var geneSearchForm=(window.webpackJsonp_name_=window.webpackJsonp_name_||[]).push([[6],{248:function(n,e,r){"use strict";r.r(e),r.d(e,"render",(function(){return i}));var a=r(1),t=r.n(a),c=r(9),o=r.n(c),d=r(78),u=r.n(d),i=function(n,e){o.a.render(t.a.createElement(u.a,n),document.getElementById(e))}}},[[248,0]]]);
+var geneSearchForm =
+(window["webpackJsonp_name_"] = window["webpackJsonp_name_"] || []).push([["geneSearchForm"],{
+
+/***/ "./bundles/gene-search-form/index.js":
+/*!*******************************************!*\
+  !*** ./bundles/gene-search-form/index.js ***!
+  \*******************************************/
+/*! exports provided: render */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var scxa_gene_search_form__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! scxa-gene-search-form */ \"./node_modules/scxa-gene-search-form/lib/index.js\");\n/* harmony import */ var scxa_gene_search_form__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(scxa_gene_search_form__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\nvar render = function render(options, target) {\n  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(scxa_gene_search_form__WEBPACK_IMPORTED_MODULE_2___default.a, options), document.getElementById(target));\n};\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9idW5kbGVzL2dlbmUtc2VhcmNoLWZvcm0vaW5kZXguanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9maXJlZm94Ly4vYnVuZGxlcy9nZW5lLXNlYXJjaC1mb3JtL2luZGV4LmpzP2U0ODEiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0J1xuaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbSdcblxuaW1wb3J0IEZldGNoTG9hZGVyIGZyb20gJ3NjeGEtZ2VuZS1zZWFyY2gtZm9ybSdcblxuY29uc3QgcmVuZGVyID0gKG9wdGlvbnMsIHRhcmdldCkgPT4ge1xuICBSZWFjdERPTS5yZW5kZXIoPEZldGNoTG9hZGVyIHsuLi5vcHRpb25zfSAvPiwgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQodGFyZ2V0KSlcbn1cblxuZXhwb3J0IHsgcmVuZGVyIH1cbiJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQ0E7QUFFQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./bundles/gene-search-form/index.js\n");
+
+/***/ })
+
+},[["./bundles/gene-search-form/index.js","vendorCommons"]]]);

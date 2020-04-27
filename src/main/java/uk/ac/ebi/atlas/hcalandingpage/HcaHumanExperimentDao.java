97257c62693c70fb9cf594161140332b3ce79b75
upendrakumbham <upendra.kumbham@gmail.com>
2020-04-27 12:47:08 +0100

    Modify Solr query to accept multiple Organism parts
    - Used streaming expressions to optimise Solr query.

--- src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaHumanExperimentDao.java
+++ src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaHumanExperimentDao.java
@@ -1,24 +1,23 @@
 package uk.ac.ebi.atlas.hcalandingpage;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import org.springframework.stereotype.Component;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.springframework.stereotype.Repository;
 import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
+import uk.ac.ebi.atlas.solr.cloud.TupleStreamer;
 import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
+import uk.ac.ebi.atlas.solr.cloud.search.streamingexpressions.decorator.UniqueStreamBuilder;
+import uk.ac.ebi.atlas.solr.cloud.search.streamingexpressions.source.SearchStreamBuilder;
+
+import java.util.Set;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
-import static org.apache.commons.lang.StringUtils.isNotBlank;
 import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.CHARACTERISTIC_NAME;
 import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.CHARACTERISTIC_VALUE;
 import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.EXPERIMENT_ACCESSION;
-import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION;
-import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION_ANCESTORS_LABELS;
-import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION_ANCESTORS_URIS;
-import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.ONTOLOGY_ANNOTATION_LABEL;
 
-@Component
+@Repository
 public class HcaHumanExperimentDao {
     private final SingleCellAnalyticsCollectionProxy singleCellAnalyticsCollectionProxy;
 
@@ -26,33 +25,32 @@ public class HcaHumanExperimentDao {
         this.singleCellAnalyticsCollectionProxy = solrCloudCollectionProxyFactory.create(SingleCellAnalyticsCollectionProxy.class);
     }
 
-    public ImmutableSet<String> fetchExperimentAccessions(String characteristicName, String characteristicValue) {
+    public ImmutableSet<String> fetchExperimentAccessions(String characteristicName,
+                                                          Set<String> characteristicValues) {
         var queryBuilder = new SolrQueryBuilder<SingleCellAnalyticsCollectionProxy>();
         /* Reason for checking blank here is in case characteristicValue is empty our solrQueryBuilder
          * builds query like characteristic_value:("\*") which is different from characteristic_value:*
          * which we need. So to handle that situation if characteristicValue is empty we build query
          * characteristic_name:("organism_part") as we need all the experiments in case of empty characteristicValue
          * */
-        queryBuilder
-                .setNormalize(false)
-                .addQueryFieldByTerm(CHARACTERISTIC_NAME, characteristicName)
+
+        queryBuilder.addQueryFieldByTerm(CHARACTERISTIC_NAME, characteristicName)
                 .setFieldList(EXPERIMENT_ACCESSION)
-                .setRows(10000000);
-        if (isNotBlank(characteristicValue)) {
-            queryBuilder
-                    .addQueryFieldByTerm(ImmutableMap.of(
-                            CHARACTERISTIC_VALUE, ImmutableList.of("*".concat(characteristicValue)),
-                            ONTOLOGY_ANNOTATION, ImmutableList.of("*".concat(characteristicValue)),
-                            ONTOLOGY_ANNOTATION_ANCESTORS_LABELS, ImmutableList.of("*".concat(characteristicValue)),
-                            ONTOLOGY_ANNOTATION_ANCESTORS_URIS, ImmutableList.of("*".concat(characteristicValue)),
-                            ONTOLOGY_ANNOTATION_LABEL, ImmutableList.of("*".concat(characteristicValue))
-                    ));
+                .sortBy(EXPERIMENT_ACCESSION, SolrQuery.ORDER.asc)
+                .setRows(SolrQueryBuilder.DEFAULT_ROWS);
+
+        if(!characteristicValues.isEmpty()) {
+            queryBuilder.addQueryFieldByTerm(CHARACTERISTIC_VALUE, characteristicValues);
         }
 
-        var results = this.singleCellAnalyticsCollectionProxy.query(queryBuilder).getResults();
-        return results
-                .stream()
-                .map(solrDocument -> (String) solrDocument.getFieldValue(EXPERIMENT_ACCESSION.name()))
-                .collect(toImmutableSet());
+        var humanExperimentsSearchStream = new SearchStreamBuilder<>(singleCellAnalyticsCollectionProxy, queryBuilder);
+
+        var uniqueHumanExperimentsStream = new UniqueStreamBuilder(humanExperimentsSearchStream, EXPERIMENT_ACCESSION.name());
+
+        try(TupleStreamer tupleStreamer = TupleStreamer.of(uniqueHumanExperimentsStream.build())) {
+            return (tupleStreamer.get()
+                    .map(tuple -> (String) tuple.get(EXPERIMENT_ACCESSION.name()))
+                    .collect(toImmutableSet()));
+        }
     }
 }

d556bbf962eec575e2cb65466f01901d11684e98
upendrakumbham <upendra.kumbham@gmail.com>
2020-08-17 08:53:59 +0100

    Implement HeatmapAdapter method for new endpoint
    We cannot use same method to return json result as we are passing different DTO(MarkerGene).

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/HighchartsHeatmapAdapter.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/HighchartsHeatmapAdapter.java
@@ -16,7 +16,11 @@ import static java.util.Comparator.comparing;
 
 @Component
 public class HighchartsHeatmapAdapter {
+    @Deprecated
     private static final Function<MarkerGene, Pair<String, Integer>> MARKER_GENE_TO_ID_CLUSTER_ID_WHERE_MARKER =
+            markerGene -> Pair.of(markerGene.geneId(), markerGene.clusterIdWhereMarker());
+
+    private static final Function<CellTypeMarkerGene, Pair<String, String>> CELL_TYPE_MARKER_GENE_TO_ID_CLUSTER_ID_WHERE_MARKER =
             markerGene -> Pair.of(markerGene.geneId(), markerGene.clusterIdWhereMarker());
 
     private final BioEntityPropertyDao bioEntityPropertyDao;
@@ -26,13 +30,14 @@ public class HighchartsHeatmapAdapter {
     }
 
     /**
-     *  Given a list of marker genes, this method returns a Highcharts data series object
-     *  (https://api.highcharts.com/highcharts/series.heatmap.data), where gene IDs/symbols are
-     *  the rows (y values), and the cluster IDs are the columns (x values).
-     *  The cells contain the median average expression of the gene in the cluster.
-     *  The rows of the heatmap are ordered by the cluster number, i.e. genes for cluster 1, 2, etc.
-     *  If there are no marker genes for a cluster, then no rows will be present in the data.
+     * Given a list of marker genes, this method returns a Highcharts data series object
+     * (https://api.highcharts.com/highcharts/series.heatmap.data), where gene IDs/symbols are
+     * the rows (y values), and the cluster IDs are the columns (x values).
+     * The cells contain the median average expression of the gene in the cluster.
+     * The rows of the heatmap are ordered by the cluster number, i.e. genes for cluster 1, 2, etc.
+     * If there are no marker genes for a cluster, then no rows will be present in the data.
      */
+    @Deprecated
     public ImmutableList<Map<String, Object>> getMarkerGeneHeatmapData(Collection<MarkerGene> markerGenes) {
         // Whether the comparison by p-value should or shouldn’t be reversed depends on the yAxis.reversed property in
         // the heatmap component. In our case it’s set to true, so lower p-value is displayed at the top without
@@ -54,14 +59,54 @@ public class HighchartsHeatmapAdapter {
 
         return sortedMarkerGenes.stream()
                 .map(markerGene ->
-                    ImmutableMap.<String, Object>builder()
-                            .put("x", markerGene.clusterId() - 1)   // Cluster IDs start at 1, Highcharts columns are 0-based
-                            .put("y", rows.indexOf(MARKER_GENE_TO_ID_CLUSTER_ID_WHERE_MARKER.apply(markerGene)))
-                            .put("geneName", symbolsForGeneIds.getOrDefault(markerGene.geneId(), markerGene.geneId()))
-                            .put("value", markerGene.medianExpression())
-                            .put("clusterIdWhereMarker", markerGene.clusterIdWhereMarker())
-                            .put("pValue", markerGene.pValue())
-                            .build())
+                        ImmutableMap.<String, Object>builder()
+                                .put("x", markerGene.clusterId() - 1)   // Cluster IDs start at 1, Highcharts columns are 0-based
+                                .put("y", rows.indexOf(MARKER_GENE_TO_ID_CLUSTER_ID_WHERE_MARKER.apply(markerGene)))
+                                .put("geneName", symbolsForGeneIds.getOrDefault(markerGene.geneId(), markerGene.geneId()))
+                                .put("value", markerGene.medianExpression())
+                                .put("clusterIdWhereMarker", markerGene.clusterIdWhereMarker())
+                                .put("pValue", markerGene.pValue())
+                                .build())
+                .collect(toImmutableList());
+    }
+
+    /**
+     * Given a list of marker genes, this method returns a Highcharts data series object
+     * (https://api.highcharts.com/highcharts/series.heatmap.data), where gene IDs/symbols are
+     * the rows (y values), and the cluster IDs are the columns (x values).
+     * The cells contain the median average expression of the gene in the cluster.
+     * The rows of the heatmap are ordered by the cluster number, i.e. genes for cluster 1, 2, etc.
+     * If there are no marker genes for a cluster, then no rows will be present in the data.
+     */
+    public ImmutableList<Map<String, Object>> getCellTypeMarkerGeneHeatmapData(Collection<CellTypeMarkerGene> markerGenes) {
+        // Whether the comparison by p-value should or shouldn’t be reversed depends on the yAxis.reversed property in
+        // the heatmap component. In our case it’s set to true, so lower p-value is displayed at the top without
+        // reversing the comparator
+        var sortedMarkerGenes = markerGenes.stream()
+                .parallel()
+                .sorted(comparing(CellTypeMarkerGene::clusterIdWhereMarker).thenComparing(CellTypeMarkerGene::pValue))
+                .collect(toImmutableList());
+
+        var rows =
+                sortedMarkerGenes.stream()
+                        .map(CELL_TYPE_MARKER_GENE_TO_ID_CLUSTER_ID_WHERE_MARKER)
+                        .distinct()
+                        .collect(toImmutableList());
+
+        var symbolsForGeneIds =
+                bioEntityPropertyDao.getSymbolsForGeneIds(
+                        sortedMarkerGenes.stream().map(CellTypeMarkerGene::geneId).collect(toImmutableSet()));
+
+        return sortedMarkerGenes.stream()
+                .map(markerGene ->
+                        ImmutableMap.<String, Object>builder()
+                                .put("x", markerGene.clusterId())   // Cluster IDs start at 1, Highcharts columns are 0-based
+                                .put("y", rows.indexOf(CELL_TYPE_MARKER_GENE_TO_ID_CLUSTER_ID_WHERE_MARKER.apply(markerGene)))
+                                .put("geneName", symbolsForGeneIds.getOrDefault(markerGene.geneId(), markerGene.geneId()))
+                                .put("value", markerGene.medianExpression())
+                                .put("clusterIdWhereMarker", markerGene.clusterIdWhereMarker())
+                                .put("pValue", markerGene.pValue())
+                                .build())
                 .collect(toImmutableList());
     }
 }

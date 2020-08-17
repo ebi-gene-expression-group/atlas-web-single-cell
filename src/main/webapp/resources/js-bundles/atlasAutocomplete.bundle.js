53f022a25b3d082adeaa21cb58a469b8520047ac
amunozpomer <amunoz@ebi.ac.uk>
2020-08-17 11:03:36 +0100

    Production bundles with latest updates

--- src/main/webapp/resources/js-bundles/atlasAutocomplete.bundle.js
+++ src/main/webapp/resources/js-bundles/atlasAutocomplete.bundle.js
@@ -1,16 +1,1 @@
-var atlasAutocomplete =
-(window["webpackJsonp_name_"] = window["webpackJsonp_name_"] || []).push([["atlasAutocomplete"],{
-
-/***/ "./bundles/autocomplete/index.js":
-/*!***************************************!*\
-  !*** ./bundles/autocomplete/index.js ***!
-  \***************************************/
-/*! exports provided: render */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var expression_atlas_autocomplete__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! expression-atlas-autocomplete */ \"./node_modules/expression-atlas-autocomplete/lib/index.js\");\n/* harmony import */ var expression_atlas_autocomplete__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(expression_atlas_autocomplete__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\nvar render = function render(options, target) {\n  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(expression_atlas_autocomplete__WEBPACK_IMPORTED_MODULE_2___default.a, options), document.getElementById(target));\n};\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9idW5kbGVzL2F1dG9jb21wbGV0ZS9pbmRleC5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovL2ZpcmVmb3gvLi9idW5kbGVzL2F1dG9jb21wbGV0ZS9pbmRleC5qcz9iNTI2Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20nXG5cbmltcG9ydCBBdGxhc0F1dG9jb21wbGV0ZSBmcm9tICdleHByZXNzaW9uLWF0bGFzLWF1dG9jb21wbGV0ZSdcblxuY29uc3QgcmVuZGVyID0gKG9wdGlvbnMsIHRhcmdldCkgPT4ge1xuICBSZWFjdERPTS5yZW5kZXIoPEF0bGFzQXV0b2NvbXBsZXRlIHsuLi5vcHRpb25zfSAvPiwgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQodGFyZ2V0KSlcbn1cblxuZXhwb3J0IHsgcmVuZGVyIH1cbiJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQ0E7QUFFQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./bundles/autocomplete/index.js\n");
-
-/***/ })
-
-},[["./bundles/autocomplete/index.js","vendorCommons"]]]);
+var atlasAutocomplete=(window.webpackJsonp_name_=window.webpackJsonp_name_||[]).push([[1],{175:function(n,e,t){"use strict";t.r(e),t.d(e,"render",(function(){return i}));var a=t(0),r=t.n(a),o=t(6),c=t.n(o),u=t(82),d=t.n(u),i=function(n,e){c.a.render(r.a.createElement(d.a,n),document.getElementById(e))}}},[[175,0]]]);

8c5a75a051b104b540dca42cdb4f2f6e05d1f4f5
upendrakumbham <upendra.kumbham@gmail.com>
2020-08-20 13:26:44 +0100

    Insert test data for the 'Not available' cell type

--- src/test/resources/fixtures/scxa_cell_group_marker_gene_stats.sql
+++ src/test/resources/fixtures/scxa_cell_group_marker_gene_stats.sql
@@ -1,1 +1,2 @@
 INSERT INTO scxa_cell_group_marker_gene_stats (gene_id, cell_group_id, marker_id, expression_type, mean_expression, median_expression) VALUES ('ENSG00000051523', 24047, 1405245, 0, 431.113537539535, 273.9726);
+INSERT INTO scxa_cell_group_marker_gene_stats (gene_id, cell_group_id, marker_id, expression_type, mean_expression, median_expression) VALUES ('ENSG00000051523', 24046, 1405245, 0, 431.113537539535, 273.9726);
--- src/test/resources/fixtures/scxa_cell_group_marker_genes.sql
+++ src/test/resources/fixtures/scxa_cell_group_marker_genes.sql
@@ -1,1 +1,3 @@
 INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405245, 'ENSG00000051523', 24047, 0);
+INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405246, 'ENSG00000051523', 24046, 0.000000);
+

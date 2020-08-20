51360f3550acbead40564790b3c71ce9898d3d8a
upendrakumbham <upendra.kumbham@gmail.com>
2020-08-20 16:12:21 +0100

    Add a test case

--- src/test/java/uk/ac/ebi/atlas/experimentpage/markergenes/HighchartsHeatmapAdapterTest.java
+++ src/test/java/uk/ac/ebi/atlas/experimentpage/markergenes/HighchartsHeatmapAdapterTest.java
@@ -60,4 +60,34 @@ class HighchartsHeatmapAdapterTest {
         assertThat(result).element(1).extracting("geneName").containsOnly(geneSymbol2);
         assertThat(result).element(2).extracting("geneName").containsOnly(gene3);
     }
+
+    @Test
+    void cellTypeMarkerGeneWithoutSymbolHasGeneIDAsName() {
+        var gene1 = generateRandomEnsemblGeneId();
+        var gene2 = generateRandomEnsemblGeneId();
+        var gene3 = generateRandomEnsemblGeneId();
+
+        var geneSymbol1 = generateRandomGeneSymbol();
+        var geneSymbol2 = generateRandomGeneSymbol();
+
+        var randomGeneIds = ImmutableSet.of(gene1, gene2, gene3);
+
+        when(bioEntityPropertyDaoMock.getSymbolsForGeneIds(randomGeneIds))
+                .thenReturn(
+                        ImmutableMap.of(
+                                gene1, geneSymbol1,
+                                gene2, geneSymbol2));
+
+        var cellTypeMarkerGenes = ImmutableList.of(
+                CellTypeMarkerGene.create(gene1, "inferred cell type", "CD8-positive, alpha-beta T cell", 0.004, "T cell", 199, 185),
+                CellTypeMarkerGene.create(gene2, "inferred cell type", "Not available", 0.0006, "Not available", 12, 1.11),
+                CellTypeMarkerGene.create(gene3, "inferred cell type", "T cell", 0.001, "B cell", 1000, 10000));
+
+        var result = subject.getCellTypeMarkerGeneHeatmapData(cellTypeMarkerGenes);
+
+        assertThat(result).hasSize(3);
+        assertThat(result).element(0).extracting("geneName").containsOnly(geneSymbol1);
+        assertThat(result).element(1).extracting("geneName").containsOnly(geneSymbol2);
+        assertThat(result).element(2).extracting("geneName").containsOnly(gene3);
+    }
 }

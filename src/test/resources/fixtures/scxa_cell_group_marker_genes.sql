c088e58c1fc4ceddbe8d06af4b3c0a25709c42dc
upendrakumbham <upendra.kumbham@gmail.com>
2020-08-20 15:39:32 +0100

    Add new cell group tables test data

--- src/test/resources/fixtures/scxa_cell_group_marker_genes.sql
+++ src/test/resources/fixtures/scxa_cell_group_marker_genes.sql
@@ -1,3 +1,6 @@
 INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405245, 'ENSG00000051523', 24047, 0);
 INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405246, 'ENSG00000051523', 24046, 0.000000);
+INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405342, 'ENSG00000008394', 24046, 0);
+INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405343, 'ENSG00000010278', 24046, 0.000);
+INSERT INTO scxa_cell_group_marker_genes (id, gene_id, cell_group_id, marker_probability) VALUES (1405344, 'ENSG00000011600', 24046, 0);
 

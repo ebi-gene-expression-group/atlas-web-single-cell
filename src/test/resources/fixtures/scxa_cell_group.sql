0b65bd3b91afba8b2cb76ce89bf6c5e51b8a34b6
upendrakumbham <upendra.kumbham@gmail.com>
2020-08-21 07:57:34 +0100

    Add space between two records just to trigger the build
    Silly me :).

--- src/test/resources/fixtures/scxa_cell_group.sql
+++ src/test/resources/fixtures/scxa_cell_group.sql
@@ -1,2 +1,3 @@
 INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24047, 'E-HCAD-8', 'inferred cell type', 'T cell');
+
 INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24046, 'E-HCAD-8', 'inferred cell type', 'Not available');

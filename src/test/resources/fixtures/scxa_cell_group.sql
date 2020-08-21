ebd99d08e78b807136f774e55bbd62145654d31d
upendrakumbham <upendra.kumbham@gmail.com>
2020-08-21 16:34:23 +0100

    Change experiment accession from E-HCAD-8 to E-EHCA-2

--- src/test/resources/fixtures/scxa_cell_group.sql
+++ src/test/resources/fixtures/scxa_cell_group.sql
@@ -1,2 +1,2 @@
-INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24047, 'E-HCAD-8', 'inferred cell type', 'T cell');
-INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24046, 'E-HCAD-8', 'inferred cell type', 'Not available');
+INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24047, 'E-EHCA-2', 'inferred cell type', 'T cell');
+INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24046, 'E-EHCA-2', 'inferred cell type', 'Not available');

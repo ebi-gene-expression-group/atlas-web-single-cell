e4951b4b508e9c79cc2f61c6f821a81745f0ba32
upendrakumbham <upendra.kumbham@gmail.com>
2020-09-07 16:54:22 +0100

    Add extra space to trigger Bamboo

--- src/test/resources/fixtures/scxa_cell_group_membership.sql
+++ src/test/resources/fixtures/scxa_cell_group_membership.sql
@@ -1,2 +1,3 @@
 INSERT INTO scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) VALUES ('E-EHCA-2', 'PP001-AAAACCGGTCTGCCAG', 24047);
+
 INSERT INTO scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) VALUES ('E-EHCA-2', 'PP001-AAACCTGAGTGTTGAA', 24046);

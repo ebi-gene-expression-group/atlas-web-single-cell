d82e424268b5fca23d5af51a9b01f751c3c433a4
amunozpomer <amunoz@ebi.ac.uk>
2020-10-16 12:43:09 +0100

    Add class that returns ontology accessions grouped by organ for a given experiment

---
+++ src/test/java/uk/ac/ebi/atlas/search/OntologyAccessionsSearchServiceIT.java
@@ -0,0 +1,36 @@
+package uk.ac.ebi.atlas.search;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import uk.ac.ebi.atlas.configuration.TestConfig;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestConfig.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OntologyAccessionsSearchServiceIT {
+    @Inject
+    private OntologyAccessionsSearchDao ontologyAccessionsSearchDao;
+
+    private OntologyAccessionsSearchService subject;
+
+    @BeforeEach
+    void setUp() {
+        subject = new OntologyAccessionsSearchService(ontologyAccessionsSearchDao);
+    }
+
+    @Test
+    void foo() {
+        assertThat(subject.searchAvailableAnnotationsForOrganAnatomogram("E-MTAB-5061")).isNotEmpty();
+        assertThat(subject.searchAvailableAnnotationsForOrganAnatomogram("E-EHCA-2")).isEmpty();
+    }
+}

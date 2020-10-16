25807338e29a6572221a938ef063bc1925dc58f2
amunozpomer <amunoz@ebi.ac.uk>
2020-10-16 12:45:16 +0100

    Add endpoint that produces anatomogram accessions grouped by organ

---
+++ src/test/java/uk/ac/ebi/atlas/experimentpage/JsonAnatomogramCellTypeHeatmapControllerWIT.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.atlas.experimentpage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JsonAnatomogramCellTypeHeatmapControllerWIT {
+
+    @Test
+    void foo() {
+        assertThat(true).isFalse();
+    }
+}

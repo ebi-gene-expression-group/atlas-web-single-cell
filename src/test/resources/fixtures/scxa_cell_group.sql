a5cb81bd3bd4e419a652fc23d5b7955246bede13
upendrakumbham <upendra.kumbham@gmail.com>
2020-10-20 13:52:01 +0100

    Add test data(E-CURD-4) for the cell group and membership tables
    Added test data for the experiment - E-CURD-4.

--- src/test/resources/fixtures/scxa_cell_group.sql
+++ src/test/resources/fixtures/scxa_cell_group.sql
@@ -1,2 +1,14 @@
 INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24047, 'E-EHCA-2', 'inferred cell type', 'T cell');
 INSERT INTO scxa_cell_group (id, experiment_accession, variable, value) VALUES (24046, 'E-EHCA-2', 'inferred cell type', 'Not available');
+-- Test data belongs to t-SNE coordinates
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (58936, 'E-CURD-4', 11, 8);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (58951, 'E-CURD-4', 15, 8);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (58968, 'E-CURD-4', 21, 4);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59002, 'E-CURD-4', 32, 6);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59012, 'E-CURD-4', 44, 15);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59064, 'E-CURD-4', 60, 22);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59113, 'E-CURD-4', 6, 4);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59125, 'E-CURD-4', 67, 18);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59186, 'E-CURD-4', 7, 4);
+insert into scxa_cell_group (id, experiment_accession, variable, value) values (59202, 'E-CURD-4', 'inferred cell type - authors labels', 'root endodermis 12');
+
--- src/test/resources/fixtures/scxa_cell_group_membership.sql
+++ src/test/resources/fixtures/scxa_cell_group_membership.sql
@@ -1,2 +1,13 @@
 INSERT INTO scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) VALUES ('E-EHCA-2', 'PP001-AAAACCGGTCTGCCAG', 24047);
 INSERT INTO scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) VALUES ('E-EHCA-2', 'PP001-AAACCTGAGTGTTGAA', 24046);
+-- Test data belongs to t-SNE coordinates membership
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 58936);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 58951);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 58968);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59002);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59012);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59064);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59113);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59125);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59186);
+insert into scxa_cell_group_membership (experiment_accession, cell_id, cell_group_id) values ('E-CURD-4', 'SRR8206654-AAATGGCATGTT', 59202);

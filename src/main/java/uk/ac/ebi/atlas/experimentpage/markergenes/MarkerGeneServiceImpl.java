635a6c96e8017404a78b8b4680cbefcfa55b3bd9
upendrakumbham <upendra.kumbham@gmail.com>
2020-10-22 12:30:50 +0100

    Remove 'Not available' cell types
    Filtering  'Not available' cell types from the cell type marker genes .

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceImpl.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceImpl.java
@@ -3,7 +3,7 @@ package uk.ac.ebi.atlas.experimentpage.markergenes;
 import com.google.common.collect.ImmutableList;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 @Service
 public class MarkerGeneServiceImpl implements MarkerGeneService {
@@ -17,11 +17,15 @@ public class MarkerGeneServiceImpl implements MarkerGeneService {
      * Get ImmutableList of marker genes objects for an experiment of that organism part.
      * @param organismPart
      * @param experimentAccession
-     * @return ImmutableList of CellTypeMarkerGene Objects
+     * @return ImmutableList of CellTypeMarkerGene  Objects
      */
     @Override
     public ImmutableList<CellTypeMarkerGene> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart) {
         List<CellTypeMarkerGene> cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, organismPart);
-        return ImmutableList.copyOf(cellTypeMarkerGenes);
+        return cellTypeMarkerGenes.stream()
+                .filter(markerGene -> !markerGene.cellGroupValue().equals("Not available"))
+                //'cellTypeWhereMarker' attribute is not feasible to filter 'Not available' cells as it
+                // contains descriptive value ,so selected 'cellType' attribute to filter 'Not available' cells.
+                .collect(toImmutableList());
     }
 }

fbe71850b6f4a9f29abb010cddb20f8dc55c7d00
upendrakumbham <upendra.kumbham@gmail.com>
2020-10-23 11:18:00 +0100

    Contact Solr to fetch cell type values
    Calling Solr to fetch cell types.

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
@@ -1,9 +1,9 @@
 package uk.ac.ebi.atlas.experimentpage.markergenes;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.atlas.search.CellTypeSearchDao;
-
 import java.util.List;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 
@@ -24,11 +24,16 @@ public class MarkerGeneService {
      * @return ImmutableList of CellTypeMarkerGene Objects
      */
     public ImmutableList<CellTypeMarkerGene> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart) {
-        List<CellTypeMarkerGene> cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, organismPart);
+        List<CellTypeMarkerGene> cellTypeMarkerGenes = null;
+        ImmutableSet<String> ontologyLabelsCellTypeValues = cellTypeSearchDao.getInferredCellTypeOntologyLabels(experimentAccession, organismPart);
+        if (ontologyLabelsCellTypeValues.isEmpty()) {
+            ImmutableSet<String> authorsLabelsCellTypeValues = cellTypeSearchDao.getInferredCellTypeAuthorsLabels(experimentAccession, organismPart);
+            cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, authorsLabelsCellTypeValues);
+        }
+        cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, ontologyLabelsCellTypeValues);
+        //If both(OntologyLabels & AuthorsLabels) returns empty cell types, front end handles this with user friendly message.
         return cellTypeMarkerGenes.stream()
                 .filter(markerGene -> !markerGene.cellGroupValue().equals("Not available"))
-                //'cellTypeWhereMarker' attribute is not feasible to filter 'Not available' cells as it
-                // contains descriptive value ,so selected 'cellType' attribute to filter 'Not available' cells.
                 .collect(toImmutableList());
     }
 }

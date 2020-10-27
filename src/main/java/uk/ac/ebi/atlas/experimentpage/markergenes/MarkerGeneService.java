c254d299bc1e4ae469fad2e3322a45022d606c41
upendrakumbham <upendra.kumbham@gmail.com>
2020-10-27 04:22:41 +0000

    Add exeption if cell types are empty

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.atlas.experimentpage.markergenes;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.atlas.controllers.BioentityNotFoundException;
 import uk.ac.ebi.atlas.search.CellTypeSearchDao;
 import java.util.List;
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -19,6 +20,7 @@ public class MarkerGeneService {
 
     /**
      * Get ImmutableList of marker genes objects for an experiment of that organism part.
+     *
      * @param organismPart
      * @param experimentAccession
      * @return ImmutableList of CellTypeMarkerGene Objects
@@ -26,12 +28,16 @@ public class MarkerGeneService {
     public ImmutableList<CellTypeMarkerGene> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart) {
         List<CellTypeMarkerGene> cellTypeMarkerGenes = null;
         ImmutableSet<String> ontologyLabelsCellTypeValues = cellTypeSearchDao.getInferredCellTypeOntologyLabels(experimentAccession, organismPart);
+
         if (ontologyLabelsCellTypeValues.isEmpty()) {
             ImmutableSet<String> authorsLabelsCellTypeValues = cellTypeSearchDao.getInferredCellTypeAuthorsLabels(experimentAccession, organismPart);
+            if (authorsLabelsCellTypeValues.isEmpty()) {
+                throw new BioentityNotFoundException("OrganismOrOrganismPart: " + organismPart + " doesn't have annotations.");
+            }//If both(OntologyLabels & AuthorsLabels) returns empty cell types, throwing this exception with the user message.
             cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, authorsLabelsCellTypeValues);
-        }
-        cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, ontologyLabelsCellTypeValues);
-        //If both(OntologyLabels & AuthorsLabels) returns empty cell types, front end handles this with user friendly message.
+        } else
+            cellTypeMarkerGenes = markerGenesDao.getCellTypeMarkerGenes(experimentAccession, ontologyLabelsCellTypeValues);
+
         return cellTypeMarkerGenes.stream()
                 .filter(markerGene -> !markerGene.cellGroupValue().equals("Not available"))
                 .collect(toImmutableList());

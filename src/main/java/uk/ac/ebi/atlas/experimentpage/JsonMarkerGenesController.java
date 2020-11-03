89d83b9e1ede3786944edd86a0465f5fc46ed646
upendrakumbham <upendra.kumbham@gmail.com>
2020-11-03 11:53:58 +0000

    Remove unused import

--- src/main/java/uk/ac/ebi/atlas/experimentpage/JsonMarkerGenesController.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/JsonMarkerGenesController.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.atlas.experimentpage;
 
-import com.google.common.collect.ImmutableSet;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

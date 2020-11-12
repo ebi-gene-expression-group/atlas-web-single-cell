e02a4bd3d479e4ccc42a933d7cb72850a9d4e21e
upendrakumbham <upendra.kumbham@gmail.com>
2020-11-12 13:45:59 +0000

    Extract a method to reuse code for cluster and celltype endpoints

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceImpl.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceImpl.java
@@ -1,8 +1,10 @@
 package uk.ac.ebi.atlas.experimentpage.markergenes;
 
 import com.google.common.collect.ImmutableSet;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
@@ -12,30 +14,22 @@ import static java.util.stream.Collectors.toMap;
 
 @Service
 public class MarkerGeneServiceImpl implements MarkerGeneService {
-    private MarkerGenesDao markerGenesDao;
+    private final MarkerGenesDao markerGenesDao;
 
     public MarkerGeneServiceImpl(MarkerGenesDao markerGenesDao) {
         this.markerGenesDao = markerGenesDao;
     }
 
 
-     /**
-     *
+    /**
      * @param experimentAccession - Id of the experiment
-     * @param organismPart - ontology label or authors label(ontology URL or authors URL)
-     * @return Map<String, ImmutableSet<MarkerGene>> of cell type as a key and top 5 MarkerGene
+     * @param organismPart        - ontology label or authors label(ontology URL or authors URL)
+     * @return Map<String, ImmutableSet < MarkerGene>> of cell type as a key and top 5 MarkerGene
      * Objects as a value
      */
     @Override
     public Map<String, ImmutableSet<MarkerGene>> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart) {
-        return markerGenesDao.getCellTypeMarkerGenes(experimentAccession, organismPart)
-                .stream()
-                .collect(groupingBy(MarkerGene::cellGroupValue, toImmutableSet())).entrySet().stream()
-                .collect(toMap(cellTypeMarkerGenes -> cellTypeMarkerGenes.getKey(),
-                        cellTypeMarkerGenes -> cellTypeMarkerGenes.getValue()
-                                .stream()
-                                .limit(5)
-                                .collect(toImmutableSet())));
+        return collectTop5MarkerGenes(markerGenesDao.getCellTypeMarkerGenes(experimentAccession, organismPart));
     }
 
     /**
@@ -46,11 +40,16 @@ public class MarkerGeneServiceImpl implements MarkerGeneService {
      */
     @Override
     public Map<String, ImmutableSet<MarkerGene>> getMarkerGenesPerCluster(String experimentAccession, String k) {
-        return markerGenesDao.getMarkerGenesWithAveragesPerCluster(experimentAccession, k)
+        return collectTop5MarkerGenes(markerGenesDao.getMarkerGenesWithAveragesPerCluster(experimentAccession, k));
+    }
+
+    @NotNull
+    private Map<String, ImmutableSet<MarkerGene>> collectTop5MarkerGenes(List<MarkerGene> markerGenesPerClusterOrPerCelltype) {
+        return markerGenesPerClusterOrPerCelltype
                 .stream()
                 .collect(groupingBy(MarkerGene::cellGroupValue, toImmutableSet())).entrySet().stream()
-                .collect(toMap(markerGenes -> markerGenes.getKey(), // cluster id as a key
-                        markerGenes -> markerGenes.getValue()// Gets only first 5 marker genes for each cluster id
+                .collect(toMap(markerGenes -> markerGenes.getKey(), // cluster id or cell type as a key
+                        markerGenes -> markerGenes.getValue()// Gets only first 5 marker genes for each cluster id or cell type
                                 .stream()
                                 .limit(5)
                                 .collect(toImmutableSet())));

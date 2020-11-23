d0f5a3517c6fa286ffe0616e8be0acf780865d17
upendrakumbham <upendra.kumbham@gmail.com>
2020-11-23 16:17:59 +0000

    Improve method
    this commit contains:
       - Return immutableMap
       - Make input more generic to accept any collection as a input
       - Good generic variable name that works for both

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceImpl.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceImpl.java
@@ -1,12 +1,15 @@
 package uk.ac.ebi.atlas.experimentpage.markergenes;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toMap;
@@ -44,11 +47,11 @@ public class MarkerGeneServiceImpl implements MarkerGeneService {
     }
 
     @NotNull
-    private Map<String, ImmutableSet<MarkerGene>> collectTop5MarkerGenes(List<MarkerGene> markerGenesPerClusterOrPerCelltype) {
-        return markerGenesPerClusterOrPerCelltype
+    private ImmutableMap<String, ImmutableSet<MarkerGene>> collectTop5MarkerGenes(Collection<MarkerGene> markerGenesPerCellGroup) {
+        return markerGenesPerCellGroup
                 .stream()
                 .collect(groupingBy(MarkerGene::cellGroupValue, toImmutableSet())).entrySet().stream()
-                .collect(toMap(markerGenes -> markerGenes.getKey(), // cluster id or cell type as a key
+                .collect(toImmutableMap(markerGenes -> markerGenes.getKey(), // cluster id or cell type as a key
                         markerGenes -> markerGenes.getValue()// Gets only first 5 marker genes for each cluster id or cell type
                                 .stream()
                                 .limit(5)

5bb968b8f522aa42eaf73442e25613d4289dd8e5
upendrakumbham <upendra.kumbham@gmail.com>
2020-11-23 16:06:47 +0000

    Remove redundant code

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.atlas.experimentpage.markergenes;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
 import java.util.Map;
@@ -11,16 +10,16 @@ public interface MarkerGeneService {
      *
      * @param experimentAccession
      * @param organismPart
-     * @return Map<String, ImmutableSet<MarkerGene>> of cell type as a key and top 5 MarkerGene
+     * @return Map<String, ImmutableSet < MarkerGene>> of cell type as a key and top 5 MarkerGene
      * Objects as a value
      */
-    public Map<String, ImmutableSet<MarkerGene>> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart);
+    Map<String, ImmutableSet<MarkerGene>> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart);
 
     /**
      * @param experimentAccession - Id of the experiment
-     * @param k - no of clusters
-     * @return Map<String, ImmutableSet<MarkerGene>> of cluster id as a key and top 5 MarkerGene
+     * @param k                   - no of clusters
+     * @return Map<String, ImmutableSet < MarkerGene>> of cluster id as a key and top 5 MarkerGene
      * Objects as a value
      */
-    public Map<String, ImmutableSet<MarkerGene>> getMarkerGenesPerCluster(String experimentAccession, String k);
+    Map<String, ImmutableSet<MarkerGene>> getMarkerGenesPerCluster(String experimentAccession, String k);
 }

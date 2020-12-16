36358224b47be35a09fe1761926c2b697059f424
upendra kumbham <ukumbham@ebi.ac.uk>
2020-12-16 17:30:03 +0000

    Updated service class

--- src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneService.java
@@ -16,35 +16,34 @@ public class MarkerGeneService {
         this.cellTypeSearchDao = cellTypeSearchDao;
     }
 
-    /**
-     * Get ImmutableList of marker genes objects for an experiment of that organism part.
-     *
-     * @param organismPart
-     * @param experimentAccession
-     * @return ImmutableList of CellTypeMarkerGene Objects
-     * @return ImmutableList<MarkerGene> of cell type marker genes
-     * Objects as a value
-     */
-    public ImmutableList<CellTypeMarkerGene> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart) {
+	/**
+	 * Get ImmutableList of marker genes objects for an experiment of that organism part.
+	 *
+	 * @param organismPart
+	 * @param experimentAccession
+	 * @return ImmutableList of CellTypeMarkerGene Objects
+	 */
+	public ImmutableList<MarkerGene> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart) {
 
-        var ontologyLabelsCellTypeValues = cellTypeSearchDao.getInferredCellTypeOntologyLabels(experimentAccession, organismPart);
+		var ontologyLabelsCellTypeValues = cellTypeSearchDao.getInferredCellTypeOntologyLabels(experimentAccession, organismPart);
 
-        var cellTypeMarkerGenes = ontologyLabelsCellTypeValues.isEmpty() ?
-                markerGenesDao.getCellTypeMarkerGenes(experimentAccession,
-                        cellTypeSearchDao.getInferredCellTypeAuthorsLabels(experimentAccession, organismPart)) :
-                markerGenesDao.getCellTypeMarkerGenes(experimentAccession, ontologyLabelsCellTypeValues);
+		var cellTypeMarkerGenes = ontologyLabelsCellTypeValues.isEmpty() ?
+				markerGenesDao.getCellTypeMarkerGenes(experimentAccession,
+						cellTypeSearchDao.getInferredCellTypeAuthorsLabels(experimentAccession, organismPart)) :
+				markerGenesDao.getCellTypeMarkerGenes(experimentAccession, ontologyLabelsCellTypeValues);
 
-        return cellTypeMarkerGenes.stream()
-                .filter(markerGene -> !markerGene.cellGroupValue().equalsIgnoreCase("Not available"))
-                .collect(toImmutableList());
-    }
-    ImmutableList<MarkerGene> getCellTypeMarkerGeneProfile(String experimentAccession, String organismPart);
+		return cellTypeMarkerGenes.stream()
+				.filter(markerGene -> !markerGene.cellGroupValue().equalsIgnoreCase("Not available"))
+				.collect(toImmutableList());
+	}
 
-    /**
-     * @param experimentAccession - Id of the experiment
-     * @param k                   - no of clusters
-     * @return ImmutableList<MarkerGene> of marker genes
-     * Objects as a value
-     */
-    ImmutableList<MarkerGene> getMarkerGenesPerCluster(String experimentAccession, String k);
+	/**
+	 * @param experimentAccession - Id of the experiment
+	 * @param k                   - no of clusters
+	 * @return Map<String, ImmutableSet < MarkerGene>> of cluster MarkerGenes
+	 * Objects as a value
+	 */
+	public ImmutableList<MarkerGene> getMarkerGenesPerCluster(String experimentAccession, String k) {
+		return ImmutableList.copyOf(markerGenesDao.getMarkerGenesWithAveragesPerCluster(experimentAccession, k));
+	}
 }

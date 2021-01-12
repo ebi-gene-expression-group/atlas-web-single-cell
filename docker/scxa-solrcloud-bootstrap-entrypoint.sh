0e9b38289361d5fa5b3032b8ad404445231e0e80
Alfonso Muñoz-Pomer Fuentes <alfonso.munozpomer@protonmail.com>
2021-01-12 15:08:58 +0000

    Wait for Solr to settle before restoring collection snapshots

--- docker/scxa-solrcloud-bootstrap-entrypoint.sh
+++ docker/scxa-solrcloud-bootstrap-entrypoint.sh
@@ -14,7 +14,6 @@ git checkout 0.5.0
 /index-scxa/bin/create-scxa-analytics-biosolr-lib.sh
 /index-scxa/bin/create-scxa-analytics-schema.sh
 
-cd /index-scxa
 git checkout 0.4.0
 /index-scxa/bin/create-scxa-analytics-config-set.sh
 /index-scxa/bin/create-scxa-analytics-collection.sh
@@ -22,7 +21,6 @@ git checkout 0.4.0
 # /index-scxa/bin/create-scxa-analytics-biosolr-lib.sh
 /index-scxa/bin/create-scxa-analytics-schema.sh
 
-cd /index-scxa
 git checkout 0.3.0
 /index-scxa/bin/create-scxa-analytics-config-set.sh
 /index-scxa/bin/create-scxa-analytics-collection.sh
@@ -30,8 +28,13 @@ git checkout 0.3.0
 # /index-scxa/bin/create-scxa-analytics-biosolr-lib.sh
 /index-scxa/bin/create-scxa-analytics-schema.sh
 
+# Give Solr a bit of time to finish setting up the collections.
+# Otherwise we could issue a restore request to a (yet) non-existing collection!
+sleep 10s
+
 # Restore all collections
 # Env variables come from Dockerfile
+
 for SOLR_HOST in $SOLR_HOSTS
 do
   for SOLR_COLLECTION in $SOLR_COLLECTIONS
@@ -41,11 +44,14 @@ do
     # Pattern enclosed in (?<=) is zero-width look-behind and (?=) is zero-width look-ahead, we match everything in between
     STATUS=`curl -s "http://${SOLR_HOST}/solr/${SOLR_COLLECTION}/replication?command=restorestatus" | grep -oP '(?<="status":").*(?=")'`
 
+    WAIT_SECONDS=1
     # We wait until "status" field is "success"
     while [ "${STATUS}" != "success" ]
     do
-      sleep 1s
+      echo "Restoring ${SOLR_COLLECTION} at ${SOLR_HOST}: ${STATUS}"
+      sleep ${WAIT_SECONDS}s
       STATUS=`curl -s "http://${SOLR_HOST}/solr/${SOLR_COLLECTION}/replication?command=restorestatus" | grep -oP '(?<="status":").*(?=")'`
+      WAIT_SECONDS=$(( WAIT_SECONDS + WAIT_SECONDS ))
     done
   done
 done

a929b5a5d440a9dc958a9db2f3e44ed7e4c89e6b
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-02-11 10:55:35 +0000

    Bugfix #176761819, #176817876 – Reset heatmap whenever the anatomogram view is changed; back icon works without any selection (#150)
    * Update cell type heatmap resource via afterSwitchView callback
    
    * Dev JS bundles

--- src/main/javascript/bundles/experiment-page/src/results/AnatomogramCellTypeHeatmapView.js
+++ src/main/javascript/bundles/experiment-page/src/results/AnatomogramCellTypeHeatmapView.js
@@ -37,6 +37,7 @@ class AnatomogramCellTypeHeatmapView extends React.Component {
     this._addRemoveFromSelectIds = this._addRemoveFromSelectIds.bind(this)
     this._showLinkBoxIds = this._showLinkBoxIds.bind(this)
     this._handleSelectOnChange = this._handleSelectOnChange.bind(this)
+    this._afterSwitchView = this._afterSwitchView.bind(this)
   }
 
   _handleSelectOnChange(event) {
@@ -46,46 +47,81 @@ class AnatomogramCellTypeHeatmapView extends React.Component {
   }
 
   _addRemoveFromSelectIds(ids) {
-    let selectedAccessionOrLinkAccession = ids
-    if (ids[0] === `link_islet_of_langerhans`) {
-      selectedAccessionOrLinkAccession = [`UBERON_0001264`]
-    } else if (ids[0] === `link_glomerulus`) {
-      selectedAccessionOrLinkAccession = [`UBERON_0000074`]
-    } else if (ids[0] === `link_duct`) {
-      selectedAccessionOrLinkAccession = [`UBERON_0001232`]
-    } else if (ids[0] === `link_nephron`) {
-      selectedAccessionOrLinkAccession = [`UBERON_0001285`]
-    } else if (ids[0] === `link_lobule_of_liver`) {
-      selectedAccessionOrLinkAccession = [ // `UBERON_0004647`,
-        `CL_0000182`, `CL_0000632`, `UBERON_0001282`, `CL_1000488`, `UBERON_0001193`, `UBERON_0006841`,
-        `UBERON_0001639`, `EFO_0010704`, `UBERON_0001281`, `CL_1000398`, `EFO_0010705`, `EFO_0010706`, `CL_0000091`
-      ]
-    } else if (ids[0] === `link_alveoli`) {
-      selectedAccessionOrLinkAccession = [
-        `UBERON_0002299`, `UBERON_0002188`, `UBERON_0016405`
-      ]
-    } else if (ids[0] === `link_segmental_bronchus`) {
-      selectedAccessionOrLinkAccession = [ //`UBERON_0002184`,
-        `CL_0002633`, `CL_1001567`,  `CL_1000271`, `CL_0000158`, `EFO_0010666`, `CL_0002370`, `UBERON_0003504`,
-        `CL_0002598`
-      ]
-    } else if (ids[0] === `link_alveolus_section`) {
-      selectedAccessionOrLinkAccession = [ // `UBERON_0003215`,
-        `CL_0002062`, `CL_0002063`, `UBERON_0003456`, `EFO_0010667`, `EFO_0010668`, `EFO_0010669`
-      ]
-    } else if (ids[0] === `link_cell_level`) {
-      selectedAccessionOrLinkAccession = [
-        `CL_2000002`, `EFO_0010710`, `CL_3000001`, `UBERON_0000371`, `UBERON_0000319`, `UBERON_0000426`,
-        `EFO_0010712`, `EFO_0010711`, `CL_0002601`, `EFO_0010708`
-      ]
+    if (ids.length === 0 || ids[0].startsWith(`link`)) {
+      this.setState({
+        selectIds: ids
+      })
+    } else {
+      this.setState({
+        selectIds: ids,
+        resource:
+          URI(`json/experiments/${this.props.experimentAccession}/marker-genes/cell-types`, this.props.host)
+            .search({organismPart: ids.map(ontologyAccessionToOntologyUri).join(`,`)})
+            .toString()
+      })
+    }
+  }
+
+  _afterSwitchView(species, view) {
+    let requestOntologyIds = []
+    if (species === `kidney`) {
+      if (view === `kidney`) {
+        requestOntologyIds = [supportedOrgans[species]]
+      } else if (view === `nephron`) {
+        requestOntologyIds = [`UBERON_0001285`]
+      } else if (view === `glomerulus`) {
+        requestOntologyIds = [`UBERON_0000074`]
+      } else if (view === `duct`) {
+        requestOntologyIds = [`UBERON_0001232`]
+      }
+    } else if (species === `liver`) {
+      if (view === `liver`) {
+        requestOntologyIds = [supportedOrgans[species]]
+      } else if (view === `lobule`) {
+        requestOntologyIds = [ // `UBERON_0004647`,
+          `CL_0000182`, `CL_0000632`, `UBERON_0001282`, `CL_1000488`, `UBERON_0001193`, `UBERON_0006841`,
+          `UBERON_0001639`, `EFO_0010704`, `UBERON_0001281`, `CL_1000398`, `EFO_0010705`, `EFO_0010706`, `CL_0000091`
+        ]
+      }
+    } else if (species === `lung`) {
+      if (view === `lung`) {
+        requestOntologyIds = [supportedOrgans[species]]
+      } else if (view === `airway`) {
+        requestOntologyIds = [ // `UBERON_0002184`,
+          `CL_0002633`, `CL_1001567`,  `CL_1000271`, `CL_0000158`, `EFO_0010666`, `CL_0002370`, `UBERON_0003504`,
+          `CL_0002598`
+        ]
+      } else if (view === `alveoli_fin`) {
+        requestOntologyIds = [ `UBERON_0002299`, `UBERON_0002188`, `UBERON_0016405` ]
+      } else if (view === `alveoli_section`) {
+        requestOntologyIds = [ // `UBERON_0003215`,
+          `CL_0002062`, `CL_0002063`, `UBERON_0003456`, `EFO_0010667`, `EFO_0010668`, `EFO_0010669`
+        ]
+      }
+    } else if (species === `pancreas`) {
+      if (view === `pancreas`) {
+        requestOntologyIds = [supportedOrgans[species]]
+      } else if (view === `acinus`) {
+        requestOntologyIds = [
+          `CL_0000173`, `CL_0000171`, `CL_0000169`, `CL_0002275`, `CL_0005019`, `CL_0002410`, `CL_0000622`
+        ]
+      }
+    } else if (species === `placenta`) {
+      if (view === `placenta`) {
+        requestOntologyIds = [supportedOrgans[species]]
+      } else if (view === `cells`) {
+        requestOntologyIds = [
+          `CL_2000002`, `EFO_0010710`, `CL_3000001`, `UBERON_0000371`, `UBERON_0000319`, `UBERON_0000426`,
+          `EFO_0010712`, `EFO_0010711`, `CL_0002601`, `EFO_0010708`
+        ]
+      }
     }
 
     this.setState({
-      selectIds: ids,
-      resource:
-        URI(`json/experiments/${this.props.experimentAccession}/marker-genes/cell-types`, this.props.host)
-          .search({organismPart: selectedAccessionOrLinkAccession.map(ontologyAccessionToOntologyUri).join(`,`)})
-          .toString()
+       resource:
+         URI(`json/experiments/${this.props.experimentAccession}/marker-genes/cell-types`, this.props.host)
+           .search({ organismPart: requestOntologyIds.map(ontologyAccessionToOntologyUri).join(`,`) })
+           .toString()
     })
   }
 
@@ -119,6 +155,7 @@ class AnatomogramCellTypeHeatmapView extends React.Component {
             onClick={this._addRemoveFromSelectIds}
             selectAllIds={[]}
             showLinkBoxIds={this._showLinkBoxIds}
+            afterSwitchView={this._afterSwitchView}
           />
         </div>
 

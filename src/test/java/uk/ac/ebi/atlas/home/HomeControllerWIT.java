e0ae39ce511c8343d9b2208f240d79571854cd05
amunozpomer <amunoz@ebi.ac.uk>
2021-02-23 16:41:18 +0000

    Rename class to match file name

--- src/test/java/uk/ac/ebi/atlas/home/HomeControllerWIT.java
+++ src/test/java/uk/ac/ebi/atlas/home/HomeControllerWIT.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = TestConfig.class)
-class HomeControllerTest {
+class HomeControllerWIT {
 
     @Autowired
     private WebApplicationContext wac;

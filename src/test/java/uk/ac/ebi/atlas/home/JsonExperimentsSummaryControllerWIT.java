a51f89cf9a4e8768210a6f8c1b3e1541f42b7af2
amunozpomer <amunoz@ebi.ac.uk>
2021-02-24 19:30:25 +0000

    Use separate data source

--- src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
+++ src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
@@ -3,8 +3,10 @@ package uk.ac.ebi.atlas.home;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,6 +20,7 @@ import uk.ac.ebi.atlas.configuration.TestConfig;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -28,10 +31,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 @ContextConfiguration(classes = TestConfig.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DirtiesContext(classMode = BEFORE_CLASS)
 class JsonExperimentsSummaryControllerWIT {
+    @Bean
+    public DataSource dataSource2() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScripts("schemas/db/shared-schema.sql", "schemas/db/gxasc-schema.sql")
+                .build();
+    }
+
     @Inject
-    private DataSource dataSource;
+    private DataSource dataSource2;
 
     @Autowired
     private WebApplicationContext wac;
@@ -42,14 +55,14 @@ class JsonExperimentsSummaryControllerWIT {
     void populateDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScripts(new ClassPathResource("fixtures/experiment-fixture.sql"));
-        populator.execute(dataSource);
+        populator.execute(dataSource2);
     }
 
     @AfterAll
     void cleanDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScripts(new ClassPathResource("fixtures/experiment-delete.sql"));
-        populator.execute(dataSource);
+        populator.execute(dataSource2);
     }
 
     @BeforeEach

1356e61fc5fc58d3fe6f7496f1e3418777d14745
amunozpomer <amunoz@ebi.ac.uk>
2021-02-27 16:56:56 +0000

    Modify test lifecycle to force-rebuild dirty (cached) context

--- src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
+++ src/test/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryControllerWIT.java
@@ -1,15 +1,12 @@
 package uk.ac.ebi.atlas.home;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,11 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.ac.ebi.atlas.configuration.TestConfig;
 
-import javax.inject.Inject;
-import javax.sql.DataSource;
-
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -30,40 +23,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = TestConfig.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Sql("/fixtures/experiment-fixture.sql")
+@Sql(value = "/fixtures/experiment-delete.sql", executionPhase = AFTER_TEST_METHOD)
 class JsonExperimentsSummaryControllerWIT {
-    @Bean
-    public DataSource dataSource2() {
-        return new EmbeddedDatabaseBuilder()
-                .generateUniqueName(true)
-                .setType(H2)
-                .setScriptEncoding("UTF-8")
-                .ignoreFailedDrops(true)
-                .addScripts("schemas/db/shared-schema.sql", "schemas/db/gxasc-schema.sql")
-                .build();
-    }
-
-    @Inject
-    private DataSource dataSource2;
-
     @Autowired
     private WebApplicationContext wac;
     private MockMvc mockMvc;
 
     private static final String ENDPOINT_URL = "/json/experiments-summary";
-    @BeforeAll
-    void populateDatabaseTables() {
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScripts(new ClassPathResource("fixtures/experiment-fixture.sql"));
-        populator.execute(dataSource2);
-    }
-
-    @AfterAll
-    void cleanDatabaseTables() {
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScripts(new ClassPathResource("fixtures/experiment-delete.sql"));
-        populator.execute(dataSource2);
-    }
 
     @BeforeEach
     void setUp() {
@@ -72,7 +39,6 @@ class JsonExperimentsSummaryControllerWIT {
 
     @Test
     void hasLatestAndFeaturedExperiments() throws Exception {
-        System.out.println(mockMvc.perform(get(ENDPOINT_URL)).andReturn().getResponse().getContentAsString());
         mockMvc.perform(get(ENDPOINT_URL))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))

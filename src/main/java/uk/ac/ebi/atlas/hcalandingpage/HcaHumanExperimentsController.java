9671cbee8d9b5d35082d91492ab2ec28757f24fd
Haider Iqbal <haideri@ebi.ac.uk>
2021-05-11 11:29:40 +0100

    Let HCA endpoint fail gracefully (#161)
    * - Add try-catch to HCA controller
    
    * Remove try,catch and use JsonExceptionHandlingController
    
    we have an existing 'JsonExceptionHandlingController' to handle controller level exceptions with proper status code like @ControllerAdvise class
    
    Co-authored-by: upendra kumbham <ukumbham@ebi.ac.uk>

--- src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaHumanExperimentsController.java
+++ src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaHumanExperimentsController.java
@@ -1,11 +1,10 @@
 package uk.ac.ebi.atlas.hcalandingpage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 import uk.ac.ebi.atlas.experiments.ExperimentJsonSerializer;
 
 import java.util.Set;
@@ -14,7 +13,7 @@ import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
-public class HcaHumanExperimentsController {
+public class HcaHumanExperimentsController extends JsonExceptionHandlingController {
     private final static String CHARACTERISTIC_NAME = "organism_part";
     private final HcaHumanExperimentService hcaHumanExperimentService;
     private final ExperimentJsonSerializer experimentJsonSerializer;
@@ -29,11 +28,11 @@ public class HcaHumanExperimentsController {
     @GetMapping(value = "/json/experiments/hca/human",
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getOrganismPartExperimentsList(@RequestParam(defaultValue = "") Set<String> organismPart) {
-        return GSON.toJson(
-                hcaHumanExperimentService
-                        .getPublicHumanExperiments(CHARACTERISTIC_NAME, organismPart)
-                        .stream()
-                        .map(experimentJsonSerializer::serialize)
-                        .collect(toImmutableSet()));
+            return GSON.toJson(
+                    hcaHumanExperimentService
+                            .getPublicHumanExperiments(CHARACTERISTIC_NAME, organismPart)
+                            .stream()
+                            .map(experimentJsonSerializer::serialize)
+                            .collect(toImmutableSet()));
+        }
     }
-}

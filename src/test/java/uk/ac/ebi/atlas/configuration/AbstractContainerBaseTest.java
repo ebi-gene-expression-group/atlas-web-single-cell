4263019c6751520709fb16a93b9bcf99ce8da96f
upendra kumbham <ukumbham@ebi.ac.uk>
2021-05-25 14:15:11 +0100

    Remove unused code

--- src/test/java/uk/ac/ebi/atlas/configuration/AbstractContainerBaseTest.java
+++ src/test/java/uk/ac/ebi/atlas/configuration/AbstractContainerBaseTest.java
@@ -3,13 +3,10 @@ package uk.ac.ebi.atlas.configuration;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 abstract class AbstractContainerBaseTest {
     static final PostgreSQLContainer TEST_DATABASE;
@@ -19,10 +16,7 @@ abstract class AbstractContainerBaseTest {
     static {
         TEST_DATABASE = new PostgreSQLContainer(DOCKER_IMAGE_VERSION);
         TEST_DATABASE.start();
-        Flyway.configure().dataSource(getDataSource(TEST_DATABASE))
-                .locations(FLY_WAY_PATH)
-                .load()
-                .migrate();
+        Flyway.configure().dataSource(getDataSource(TEST_DATABASE)).locations(FLY_WAY_PATH).load().migrate();
     }
 
     protected static DataSource getDataSource(JdbcDatabaseContainer<?> container) {
@@ -33,21 +27,5 @@ abstract class AbstractContainerBaseTest {
         hikariConfig.setDriverClassName(container.getDriverClassName());
         return new HikariDataSource(hikariConfig);
     }
-
-
-}	
-
-class FirstTest extends AbstractContainerBaseTest {	
-
-    @Test	
-    void someTestMethod() {	
-        String url = TEST_DATABASE.getJdbcUrl();
-        System.out.println("url:"+url);	
-        System.out.println("username:"+TEST_DATABASE.getUsername());
-        System.out.println("host:"+TEST_DATABASE.getContainerIpAddress());
-        System.out.println("flyway:"+TEST_DATABASE.getLogs());
-//         create a connection and run test as normal	
-        assertTrue(TEST_DATABASE.isRunning());
-    }	
-}	
+}
 

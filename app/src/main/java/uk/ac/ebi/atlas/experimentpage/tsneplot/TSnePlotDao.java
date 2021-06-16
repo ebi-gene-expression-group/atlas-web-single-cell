189f16df447d3b8377cb8083adf6cc4a5ac6fbe7
lingyun1010 <zhaolingyun1010@gmail.com>
2021-06-16 06:54:09 +0100

    Replace all tsne table with coords table

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/tsneplot/TSnePlotDao.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/tsneplot/TSnePlotDao.java
@@ -2,35 +2,39 @@ package uk.ac.ebi.atlas.experimentpage.tsneplot;
 
 import com.google.common.collect.ImmutableMap;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uk.ac.ebi.atlas.experimentpage.tsne.TSnePoint;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class TSnePlotDao {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final String TSNE_METHOD = "tsne";
 
     public TSnePlotDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     private static final String SELECT_T_SNE_PLOT_WITH_EXPRESSION_STATEMENT =
-            "SELECT tsne.cell_id, tsne.x, tsne.y, analytics.expression_level " +
-                    "FROM scxa_tsne AS tsne " +
-                    "LEFT JOIN " +
-                    "(SELECT * FROM scxa_analytics WHERE gene_id=:gene_id and experiment_accession=:experiment_accession) AS analytics " +
-                    "ON analytics.cell_id=tsne.cell_id " +
-                    "WHERE tsne.experiment_accession=:experiment_accession AND tsne.perplexity=:perplexity";
+            "SELECT c.cell_id, c.x, c.y, a.expression_level " +
+                    "FROM scxa_coords c LEFT JOIN scxa_analytics a ON " +
+                    "c.experiment_accession=a.experiment_accession AND " +
+                    "c.cell_id=a.cell_id AND a.gene_id=:gene_id " +
+                    "WHERE c.experiment_accession=:experiment_accession AND " +
+                    "c.method=:method AND c.parameterisation->0->> :parameter_name =:parameter " +
+                    "ORDER BY c.cell_id";
 
     @Transactional(transactionManager = "txManager", readOnly = true)
-    public List<TSnePoint.Dto> fetchTSnePlotWithExpression(String experimentAccession, int perplexity, String geneId) {
+    public List<TSnePoint.Dto> fetchTSnePlotWithExpression(String experimentAccession, String method, int parameter, String geneId) {
         var namedParameters =
                 ImmutableMap.of(
                         "experiment_accession", experimentAccession,
-                        "perplexity", perplexity,
+                        "parameter", String.valueOf(parameter),
+                        "method", method,
+                        "parameter_name", method == TSNE_METHOD ? "perplexity" : "n_neighbors",
                         "gene_id", geneId);
 
         return namedParameterJdbcTemplate.query(
@@ -44,39 +48,48 @@ public class TSnePlotDao {
     }
 
     private static final String SELECT_T_SNE_PLOT_WITH_CLUSTERS_STATEMENT =
-			"SELECT tsne.cell_id, tsne.x, tsne.y, clusters.cluster_id" +
-					"            FROM scxa_tsne AS tsne" +
-					"                LEFT JOIN" +
-					"                (SELECT mem.*, g.value as cluster_id FROM scxa_cell_group_membership as mem JOIN  scxa_cell_group g" +
-					"                    on mem.cell_group_id = g.id WHERE g.variable = :k AND mem.experiment_accession = :experiment_accession) AS clusters" +
-					"                ON clusters.cell_id=tsne.cell_id " +
-					"            WHERE tsne.experiment_accession = :experiment_accession AND tsne.perplexity = :perplexity";
+            "SELECT c.cell_id,c.x,c.y, g.value as cluster_id " +
+                    "FROM scxa_cell_group g " +
+                    "JOIN scxa_cell_group_membership m ON " +
+                    "g.id = m.cell_group_id AND " +
+                    "g.experiment_accession = :experiment_accession AND " +
+                    "g.variable = :variable " +
+                    "RIGHT JOIN scxa_coords c ON " +
+                    "m.cell_id=c.cell_id AND " +
+                    "m.experiment_accession=c.experiment_accession " +
+                    "WHERE c.method=:method AND c.parameterisation->0->>:parameter_name=:parameter " +
+                    "AND c.experiment_accession=:experiment_accession";
 
     @Transactional(transactionManager = "txManager", readOnly = true)
-    public List<TSnePoint.Dto> fetchTSnePlotWithClusters(String experimentAccession, int perplexity, int k) {
+    public List<TSnePoint.Dto> fetchTSnePlotWithClusters(String experimentAccession, String method, int parameter, String variable) {
         var namedParameters =
                 ImmutableMap.of(
                         "experiment_accession", experimentAccession,
-                        "perplexity", perplexity,
-                        "k", String.valueOf(k));
+                        "parameter", String.valueOf(parameter),
+                        "parameter_name", method == TSNE_METHOD ? "perplexity" : "n_neighbors",
+                        "method", method,
+                        "variable", variable);
 
         return namedParameterJdbcTemplate.query(
                 SELECT_T_SNE_PLOT_WITH_CLUSTERS_STATEMENT,
                 namedParameters,
                 (rs, rowNum) -> TSnePoint.Dto.create(
-                        rs.getDouble("x"), rs.getDouble("y"), rs.getInt("cluster_id"), rs.getString("cell_id")));
+                        rs.getDouble("x"), rs.getDouble("y"),
+                        rs.getString("cluster_id"), rs.getString("cell_id")));
     }
 
     private static final String SELECT_T_SNE_PLOT_WITHOUT_CLUSTERS_STATEMENT =
-            "SELECT tsne.cell_id, tsne.x, tsne.y " +
-                    "FROM scxa_tsne AS tsne " +
-                    "WHERE tsne.experiment_accession=:experiment_accession AND tsne.perplexity=:perplexity";
+            "SELECT coords.cell_id, coords.x, coords.y " +
+                    "FROM scxa_coords AS coords " +
+                    "WHERE coords.experiment_accession=:experiment_accession AND coords.method=:method" +
+                    "AND coords.parameterisation->0->>'perplexity'= :perplexity";
 
     public List<TSnePoint.Dto> fetchTSnePlotForPerplexity(String experimentAccession, int perplexity) {
         var namedParameters =
                 ImmutableMap.of(
                         "experiment_accession", experimentAccession,
-                        "perplexity", perplexity);
+                        "method", TSNE_METHOD,
+                        "perplexity", String.valueOf(perplexity));
 
         return namedParameterJdbcTemplate.query(
                 SELECT_T_SNE_PLOT_WITHOUT_CLUSTERS_STATEMENT,
@@ -86,19 +99,26 @@ public class TSnePlotDao {
     }
 
     private static final String SELECT_DISTINCT_PERPLEXITIES_STATEMENT =
-            "SELECT DISTINCT perplexity FROM scxa_tsne WHERE experiment_accession=:experiment_accession";
+            "SELECT DISTINCT value " +
+                    "FROM scxa_coords, lateral jsonb_each(parameterisation->0) " +
+                    "WHERE experiment_accession=:experiment_accession AND method=:method";
 
     public List<Integer> fetchPerplexities(String experimentAccession) {
-        var namedParameters = ImmutableMap.of("experiment_accession", experimentAccession);
+        var namedParameters = ImmutableMap.of(
+                "experiment_accession", experimentAccession,
+                "method", TSNE_METHOD);
 
         return namedParameterJdbcTemplate.queryForList(
                 SELECT_DISTINCT_PERPLEXITIES_STATEMENT,
                 namedParameters,
-                Integer.class);
+                String.class)
+                .stream()
+                .map(perplexity -> Integer.valueOf(perplexity))
+                .collect(Collectors.toList());
     }
 
     private static final String COUNT_CELLS_BY_EXPERIMENT_ACCESSION =
-            "SELECT COUNT(DISTINCT(cell_id)) FROM scxa_tsne WHERE experiment_accession=:experiment_accession";
+            "SELECT COUNT(DISTINCT(cell_id)) FROM scxa_coords WHERE experiment_accession=:experiment_accession";
 
     public Integer fetchNumberOfCellsByExperimentAccession(String experimentAccession) {
         var namedParameters = ImmutableMap.of("experiment_accession", experimentAccession);

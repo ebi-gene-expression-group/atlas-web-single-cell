f6d10ce09fb0f06f0beea17b94ad49e9f06d35ac
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-07-23 09:53:09 +0100

    Add stopwatch to measure experiment page steps

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/ExperimentController.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/ExperimentController.java
@@ -1,18 +1,24 @@
 package uk.ac.ebi.atlas.experimentpage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
+import uk.ac.ebi.atlas.download.FileDownloadController;
 import uk.ac.ebi.atlas.experimentpage.tabs.ExperimentPageContentSerializer;
 import uk.ac.ebi.atlas.experimentpage.tsneplot.TSnePlotSettingsService;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 @Controller
 public class ExperimentController extends HtmlExceptionHandlingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileDownloadController.class);
+
     private final ExperimentTrader experimentTrader;
     private final ExperimentPageContentSerializer experimentPageContentSerializer;
     private final ExperimentAttributesService experimentAttributesService;
@@ -33,18 +39,29 @@ public class ExperimentController extends HtmlExceptionHandlingController {
     public String showExperimentPage(Model model,
                                      @PathVariable String experimentAccession,
                                      @RequestParam(defaultValue = "") String accessKey) {
+        var stopwatch = new StopWatch();
+
+        stopwatch.start("Get experiment from trader");
         var experiment = experimentTrader.getExperiment(experimentAccession, accessKey);
+        stopwatch.stop();
 
+        stopwatch.start("Get experiment attributes");
         model.addAllAttributes(experimentAttributesService.getAttributes(experiment));
+        stopwatch.stop();
 
+        stopwatch.start("Get and serialise experiment content");
         model.addAttribute(
                 "content",
                 experimentPageContentSerializer.experimentPageContentForExperiment(experimentAccession, accessKey));
+        stopwatch.stop();
 
+        stopwatch.start("Get number of cells");
         model.addAttribute(
                 "numberOfCells",
                 tSnePlotSettingsService.getCellCount(experimentAccession));
+        stopwatch.stop();
 
+        LOGGER.debug(stopwatch.prettyPrint());
         return "experiment-page";
     }
 }

1ff68ba2741edb710af9ac2f52523b520f138fa6
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-07-23 14:27:31 +0100

    Add logged stopwatch to ExperimentPageContentSerializer

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/tabs/ExperimentPageContentSerializer.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/tabs/ExperimentPageContentSerializer.java
@@ -2,8 +2,12 @@ package uk.ac.ebi.atlas.experimentpage.tabs;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import uk.ac.ebi.atlas.experimentpage.ExperimentController;
 import uk.ac.ebi.atlas.model.experiment.ExperimentDesignTable;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
@@ -11,6 +15,8 @@ import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @Component
 public class ExperimentPageContentSerializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExperimentPageContentSerializer.class);
+
     private final ExperimentTrader experimentTrader;
     private final ExperimentPageContentService experimentPageContentService;
 
@@ -22,6 +28,8 @@ public class ExperimentPageContentSerializer {
 
     @Cacheable(cacheNames = "jsonExperimentPageTabs", key = "#experimentAccession")
     public String experimentPageContentForExperiment(String experimentAccession, final String accessKey) {
+        var stopwatch = new StopWatch(this.getClass().getSimpleName());
+
         var experiment = experimentTrader.getExperiment(experimentAccession, accessKey);
         var result = new JsonObject();
 
@@ -32,25 +40,39 @@ public class ExperimentPageContentSerializer {
 
         var availableTabs = new JsonArray();
 
+        stopwatch.start("Add tab Results");
         var results = experimentPageContentService.getTsnePlotData(experiment.getAccession());
         availableTabs.add(customContentTab("results", "Results", results));
+        stopwatch.stop();
 
+        stopwatch.start("Add tab Experiment Design");
         var experimentDesignJson =
                 experimentPageContentService.getExperimentDesign(
                         experiment.getAccession(), new ExperimentDesignTable(experiment).asJson(), accessKey);
         availableTabs.add(customContentTab("experiment-design", "Experiment Design", experimentDesignJson));
+        stopwatch.stop();
 
+        stopwatch.start("Add tab Supplementary Information");
         var sections = new JsonObject();
         sections.add("sections", experimentPageContentService.getSupplementaryInformation(experiment.getAccession()));
         availableTabs.add(customContentTab("supplementary-information", "Supplementary Information", sections));
+        stopwatch.stop();
 
+        stopwatch.start("Add tab Downloads");
         var data = new JsonObject();
         data.add("data", experimentPageContentService.getDownloads(experiment.getAccession(), accessKey));
         availableTabs.add(customContentTab("downloads", "Downloads", data));
+        stopwatch.stop();
 
         result.add("tabs", availableTabs);
 
-        return GSON.toJson(result);
+        stopwatch.start("Serialize tabs to JSON");
+        var jsonResult = GSON.toJson(result);
+        stopwatch.stop();
+
+        LOGGER.debug(stopwatch.prettyPrint());
+
+        return jsonResult;
     }
 
     private JsonObject customContentTab(String tabType, String name, JsonObject props) {

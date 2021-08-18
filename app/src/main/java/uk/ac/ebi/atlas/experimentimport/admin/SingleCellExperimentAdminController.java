19ead8222b1178b5e298fcd18e1ce4f5c8ff20ae
upendra kumbham <upendra.kumbham@gmail.com>
2021-08-18 16:28:22 +0100

    Add a fooBar endpoint to clear privateExperiments cache

--- app/src/main/java/uk/ac/ebi/atlas/experimentimport/admin/SingleCellExperimentAdminController.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentimport/admin/SingleCellExperimentAdminController.java
@@ -1,7 +1,10 @@
 package uk.ac.ebi.atlas.experimentimport.admin;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.ac.ebi.atlas.experimentimport.ScxaExperimentCrud;
 import uk.ac.ebi.atlas.resource.DataFileHub;
@@ -16,5 +19,12 @@ public class SingleCellExperimentAdminController extends ExperimentAdminControll
                 new ExperimentOps(
                         new ExperimentOpLogWriter(dataFileHub),
                         new SingleCellOpsExecutionService(scxaExperimentCrud)));
+    }
+
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "experimentAccessions", allEntries = true)})
+    @GetMapping(value = "/json/experiments/private/experimentAccessions/clearCache")
+    public String fooBar() {
+        return "";
     }
 }

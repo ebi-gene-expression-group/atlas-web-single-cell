dde7593897fbc0ecf4b94c86352136c3a32cfacc
upendra kumbham <upendra.kumbham@gmail.com>
2021-09-08 12:09:08 +0100

    Add scxa analytics suggeters dao and service implementation
    Implementation to contact Solr to fetch scxa analytics suggesters by ontology annotation labels

---
+++ app/src/main/java/uk/ac/ebi/atlas/search/AnalyticsSuggesterServiceImpl.java
@@ -0,0 +1,37 @@
+package uk.ac.ebi.atlas.search;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.solr.client.solrj.response.Suggestion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.ebi.atlas.search.suggester.AnalyticsSuggesterDao;
+import uk.ac.ebi.atlas.search.suggester.AnalyticsSuggesterService;
+import uk.ac.ebi.atlas.species.Species;
+import uk.ac.ebi.atlas.species.SpeciesFactory;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class AnalyticsSuggesterServiceImpl implements AnalyticsSuggesterService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnalyticsSuggesterServiceImpl.class);
+
+    private final AnalyticsSuggesterDao analyticsSuggesterDao;
+    private final SpeciesFactory speciesFactory;
+    private static final Function<Suggestion, Map<String, String>> SUGGESTION_TO_MAP = suggestion -> ImmutableMap.of(
+            "term", suggestion.getTerm(), "category", suggestion.getPayload());
+
+    public AnalyticsSuggesterServiceImpl(AnalyticsSuggesterDao analyticsSuggesterDao, SpeciesFactory speciesFactory) {
+        this.analyticsSuggesterDao = analyticsSuggesterDao;
+        this.speciesFactory = speciesFactory;
+    }
+
+    @Override
+    public Stream<Map<String, String>> fetchOntologyAnnotationSuggestions(String query, String... species) {
+        Species[] speciesArray = Arrays.stream(species).map(speciesFactory::create).toArray(Species[]::new);
+        var response = analyticsSuggesterDao.fetchOntologyAnnotationSuggestions(query, 100, speciesArray);
+        LOGGER.debug("suggestions:" + response.map(SUGGESTION_TO_MAP).count());
+        return response.map(SUGGESTION_TO_MAP);
+    }
+}

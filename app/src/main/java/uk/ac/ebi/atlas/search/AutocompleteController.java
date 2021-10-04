be69c78545ebd99aa8c9bcb3cafa2065da9c7bfa
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-10-04 10:18:07 +0100

    Format code

--- app/src/main/java/uk/ac/ebi/atlas/search/AutocompleteController.java
+++ app/src/main/java/uk/ac/ebi/atlas/search/AutocompleteController.java
@@ -80,10 +80,9 @@ public class AutocompleteController extends JsonExceptionHandlingController {
     @ResponseStatus(HttpStatus.OK)
     public String fetchMetaDataSuggestions( @RequestParam(value = "query") String query,
                                             @RequestParam(value = "species", required = false, defaultValue = "") String species){
-        return GSON.toJson(SolrSuggestionReactSelectAdapter.metaDataSerialize(
+      return GSON.toJson(SolrSuggestionReactSelectAdapter.metaDataSerialize(
                 analyticsSuggesterService.fetchOntologyAnnotationSuggestions(
-                query,species.split(",")))
-        );
+                query,species.split(","))));
     }
 
     private String getter() {

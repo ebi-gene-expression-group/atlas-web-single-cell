53fde455974ab826e04919a431f78dcfee9da71b
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-10-05 14:24:43 +0100

    Rename testcs method

--- app/src/test/java/uk/ac/ebi/atlas/search/AnalyticsSuggesterServiceTest.java
+++ app/src/test/java/uk/ac/ebi/atlas/search/AnalyticsSuggesterServiceTest.java
@@ -3,7 +3,11 @@ package uk.ac.ebi.atlas.search;
 import org.apache.solr.client.solrj.response.Suggestion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import uk.ac.ebi.atlas.search.suggester.AnalyticsSuggesterDao;
 import uk.ac.ebi.atlas.search.suggester.AnalyticsSuggesterService;
 import uk.ac.ebi.atlas.species.SpeciesFactory;
@@ -19,6 +23,8 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class AnalyticsSuggesterServiceTest {
     @Mock
     private AnalyticsSuggesterDao suggesterDaoMock;

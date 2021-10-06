5b824db7dce835839b87ff4dd3538ec5d58a6720
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-10-06 11:22:51 +0100

    Break SQL queries to make them more readable

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGenesDao.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGenesDao.java
@@ -10,24 +10,37 @@ import java.util.List;
 @Repository
 public class MarkerGenesDao {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-	private static final String CELL_GROUP_TYPE = "inferred cell type - ontology labels";
+    private static final String CELL_GROUP_TYPE = "inferred cell type - ontology labels";
 
-	public MarkerGenesDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    public MarkerGenesDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     private static final String SELECT_MARKER_GENES_WITH_AVERAGES_PER_CLUSTER =
-            "SELECT g.experiment_accession, m.gene_id, " +
-                    "g.variable as k_where_marker, h.value as cluster_id_where_marker, " +
-                    "g.value as cluster_id, m.marker_probability as marker_p_value, " +
-                    "s.mean_expression, s.median_expression " +
-                    "FROM " +
-                    "scxa_cell_group_marker_gene_stats s, scxa_cell_group_marker_genes m, " +
-                    "scxa_cell_group g, scxa_cell_group h " +
-                    "WHERE s.cell_group_id = g.id and s.marker_id = m.id and " +
-                    "m.cell_group_id = h.id and g.experiment_accession = :experiment_accession and " +
-                    "m.marker_probability < 0.05 and g.variable = :k and " +
-                    "expression_type = 0 order by m.marker_probability ";
+            "SELECT " +
+                    "g.experiment_accession, " +
+                    "m.gene_id, " +
+                    "m.marker_probability AS marker_p_value, " +
+                    "g.variable AS k_where_marker, " +
+                    "g.value AS cluster_id, " +
+                    "h.value AS cluster_id_where_marker, " +
+                    "s.mean_expression, " +
+                    "s.median_expression " +
+            "FROM " +
+                    "scxa_cell_group_marker_gene_stats s, " +
+                    "scxa_cell_group_marker_genes m, " +
+                    "scxa_cell_group g, " +
+                    "scxa_cell_group h " +
+            "WHERE " +
+                    "s.cell_group_id = g.id AND " +
+                    "s.marker_id = m.id AND " +
+                    "m.cell_group_id = h.id AND " +
+                    "g.experiment_accession = :experiment_accession AND " +
+                    "m.marker_probability < 0.05 AND " +
+                    "g.variable = :k AND " +
+                    "expression_type = 0 " +
+            "ORDER BY " +
+                    "m.marker_probability";
 
     public List<MarkerGene> getMarkerGenesWithAveragesPerCluster(String experimentAccession, String k) {
         var namedParameters =
@@ -65,36 +78,48 @@ public class MarkerGenesDao {
 
     private static final String SELECT_MARKER_GENES_WITH_AVERAGES_PER_CELL_GROUP =
             "SELECT " +
-                    "g.experiment_accession, m.gene_id, g.variable as cell_group_type, " +
-                    "h.value as cell_group_value_where_marker, g.value as cell_group_value, " +
-                    "m.marker_probability as marker_p_value, s.mean_expression, s.median_expression " +
-                    "FROM " +
-                    "scxa_cell_group_marker_gene_stats s, scxa_cell_group_marker_genes m, " +
-                    "scxa_cell_group g, scxa_cell_group h " +
-                    "WHERE " +
-                    "s.cell_group_id = g.id and s.marker_id = m.id and " +
-                    "m.cell_group_id = h.id and g.experiment_accession = :experiment_accession and " +
-                    "m.marker_probability < 0.05 and g.variable = :variable and " +
-                    "g.value IN (:values) and expression_type = 0 order by m.marker_probability ";
+                    "g.experiment_accession, " +
+                    "m.gene_id, " +
+                    "g.variable AS cell_group_type, " +
+                    "h.value AS cell_group_value_where_marker, " +
+                    "g.value AS cell_group_value, " +
+                    "m.marker_probability AS marker_p_value, " +
+                    "s.mean_expression, " +
+                    "s.median_expression " +
+            "FROM " +
+                    "scxa_cell_group_marker_gene_stats s, " +
+                    "scxa_cell_group_marker_genes m, " +
+                    "scxa_cell_group g, " +
+                    "scxa_cell_group h " +
+            "WHERE " +
+                    "s.cell_group_id = g.id AND " +
+                    "s.marker_id = m.id AND " +
+                    "m.cell_group_id = h.id AND " +
+                    "g.experiment_accession = :experiment_accession AND " +
+                    "m.marker_probability < 0.05 AND " +
+                    "g.variable = :variable AND " +
+                    "g.value IN (:values) AND " +
+                    "expression_type = 0 " +
+            "ORDER BY " +
+                    "m.marker_probability ";
 
-	public List<MarkerGene> getCellTypeMarkerGenes(String experiment_accession, ImmutableSet<String> cellGroupValues) {
+    public List<MarkerGene> getCellTypeMarkerGenes(String experiment_accession, ImmutableSet<String> cellGroupValues) {
+        var namedParameters =
+                ImmutableMap.of(
+                        "experiment_accession", experiment_accession,
+                        "variable", CELL_GROUP_TYPE,
+                        "values", cellGroupValues.isEmpty() ? "" : cellGroupValues);
 
-		var namedParameters =
-				ImmutableMap.of(
-						"experiment_accession", experiment_accession,
-						"variable", CELL_GROUP_TYPE,
-						"values", cellGroupValues.isEmpty() ? "" : cellGroupValues);
-
-		return namedParameterJdbcTemplate.query(
-				SELECT_MARKER_GENES_WITH_AVERAGES_PER_CELL_GROUP,
-				namedParameters,
-				(resultSet, rowNumber) -> MarkerGene.create(
-						resultSet.getString("gene_id"),
-						resultSet.getString("cell_group_type"),
-						resultSet.getString("cell_group_value_where_marker"),
-						resultSet.getDouble("marker_p_value"),
-						resultSet.getString("cell_group_value"),
-						resultSet.getDouble("median_expression"),
-						resultSet.getDouble("mean_expression")));
-	}
+        return namedParameterJdbcTemplate.query(
+                SELECT_MARKER_GENES_WITH_AVERAGES_PER_CELL_GROUP,
+                namedParameters,
+                (resultSet, rowNumber) -> MarkerGene.create(
+                        resultSet.getString("gene_id"),
+                        resultSet.getString("cell_group_type"),
+                        resultSet.getString("cell_group_value_where_marker"),
+                        resultSet.getDouble("marker_p_value"),
+                        resultSet.getString("cell_group_value"),
+                        resultSet.getDouble("median_expression"),
+                        resultSet.getDouble("mean_expression")));
+    }
 }

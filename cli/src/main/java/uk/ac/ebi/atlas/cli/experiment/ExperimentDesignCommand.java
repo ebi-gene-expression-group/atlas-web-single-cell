fa7a3a94a16ea523cfd35874de94c54d5765a14e
Pablo Moreno <pcm32@users.noreply.github.com>
2022-02-22 16:39:28 +0000

    Atlas single cell web cli (#222)
    * Atlas single cell web cli
    
    * Fix cli test and remove more unnecessary things.
    
    * Wrong deps
    
    * CreateExperiment cli and handle failed accessions
    
    * Log success with accessKeyGenerated
    
    * Explains that create also updates.
    
    * A bit of refactoring
    
    * Review suggestions
    
    * Remove unneeded tests that were just dragged.
    
    * Pjn to web core with atlas-bioentities pointer
    
    * Be able to give only private accessions if needed.
    
    * Missing deps compared to web-bulk cli PR
    
    * Most closely resemble PR https://github.com/ebi-gene-expression-group/atlas-web-bulk/pull/88
    
    * Latest web core and attempt profile !Cli for currently offending part
    
    * More profile additions that end up producing more complicated errors.
    
    * Remove any Spring dependencies from parent plug-in
    We want app to include Spring framework and CLI to include Spring Boot
    
    * Don’t include web config (i.e. ServletContext-dependent classes) in the CLI module
    
    * Avoid null pointer exception by makings sure collections are not null.
    
    Co-authored-by: Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>

---
+++ cli/src/main/java/uk/ac/ebi/atlas/cli/experiment/ExperimentDesignCommand.java
@@ -0,0 +1,42 @@
+package uk.ac.ebi.atlas.cli.experiment;
+
+import org.springframework.stereotype.Component;
+import picocli.CommandLine.Command;
+import uk.ac.ebi.atlas.cli.AbstractPerAccessionCommand;
+import uk.ac.ebi.atlas.experimentimport.ScxaExperimentCrud;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+
+@Component
+@Command(
+        name = "update-experiment-design",
+        description = "Update experiment design for a set of accessions")
+public class ExperimentDesignCommand extends AbstractPerAccessionCommand implements Callable<Integer> {
+    private static final Logger LOGGER = Logger.getLogger(ExperimentDesignCommand.class.getName());
+
+    private final ScxaExperimentCrud experimentCrud;
+
+    public ExperimentDesignCommand(ScxaExperimentCrud scxaExperimentCrud) {
+        this.experimentCrud = scxaExperimentCrud;
+    }
+
+    @Override
+    public Integer call() {
+
+        LOGGER.info("Starting update experiment designs for accessions.");
+        var failedAccessions = new ArrayList<String>();
+        for(String accession : experimentAccessions) {
+            try {
+                experimentCrud.updateExperimentDesign(accession);
+            } catch (RuntimeException e) {
+                failedAccessions.add(accession);
+                LOGGER.severe(String.format("%s failed: %s",accession, e.getMessage() ));
+            }
+        }
+
+        return handleFailedAccessions(failedAccessions);
+    }
+}

a6beeededaffd68185ae055bb93ee79aef892a15
lingyun1010 <zhaolingyun1010@gmail.com>
2022-03-30 11:55:55 +0100

    To fix the Mockito stunbbing erros

--- app/src/test/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceTest.java
+++ app/src/test/java/uk/ac/ebi/atlas/experimentpage/markergenes/MarkerGeneServiceTest.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class MarkerGeneServiceTest {
     @Mock
     private CellTypeSearchDao cellTypeSearchDaoMock;

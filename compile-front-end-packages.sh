a60b11f77bd18d27ed9be314f8f26bed654cf7e2
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-06-28 16:37:33 +0100

    Improve script that generates bundles

--- compile-front-end-packages.sh
+++ compile-front-end-packages.sh
@@ -8,44 +8,65 @@ do
   fi
 done
 
-cd app/src/main/javascript
-pushd .
+WEBPACK_OPTS="--mode development --devtool source-map"
+while getopts ":iud" opt; do
+  case $opt in
+    i)
+      INIT=true
+      ;;
+    u)
+      UPGRADE=true
+      ;;
+    p)
+      WEBPACK_OPTS="--mode production"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
+function update_npm_package {
+  if [ "$UPGRADE" = true ]; then
+      echo ">> $PWD$ ncu /@ebi-gene-expression-group/ --pre 1 -u"
+      ncu /@ebi-gene-expression-group/ --pre 1 -u
+    fi
+    if [ "$INIT" = true ]; then
+      echo ">> $PWD$ rm -rf node_modules package-lock.json"
+      rm -rf node_modules package-lock.json
+    fi
+    echo ">> $PWD$ npm install"
+    npm install
+    echo ">> $PWD$ npm audit fix"
+    npm audit fix
+}
+
+cd app/src/main/javascript
+
+pushd .
 cd modules
 for MODULE_DIR in `ls`
 do
   pushd .
   cd $MODULE_DIR
-  #ncu /@ebi-gene-expression-group/ -u
-  #rm -rf node_modules package-lock.json
-  npm install
-  npm audit fix
+  update_npm_package
+  echo ">> $PWD$ npm run prepare"
   npm run prepare
   popd
 done
-
 popd
-pushd .
 
+pushd .
 cd bundles
 for BUNDLE_DIR in `ls`
 do
   pushd .
   cd $BUNDLE_DIR
-  #ncu /@ebi-gene-expression-group/ -u
-  #rm -rf node_modules package-lock.json
-  npm install
-  npm audit fix
+  update_npm_package
   popd
 done
-
 popd
 
-WEBPACK_OPTS=${1:-"--mode development --devtool source-map"}
-
-ncu /@ebi-gene-expression-group/ -u
-#rm -rf node_modules package-lock.json
-npm install
-npm audit fix
+update_npm_package
+echo ">> $PWD$ npx webpack $WEBPACK_OPTS"
 npx webpack $WEBPACK_OPTS
-

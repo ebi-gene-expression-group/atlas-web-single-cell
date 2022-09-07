8e0e921b03e9b032f491e787ef8f2ce96a18e700
Karoly Erdos <karoly@ebi.ac.uk>
2022-09-07 11:32:05 +0100

    Fix imports in JsonGeneSearchController

--- app/src/main/java/uk/ac/ebi/atlas/search/JsonGeneSearchController.java
+++ app/src/main/java/uk/ac/ebi/atlas/search/JsonGeneSearchController.java
@@ -16,12 +16,14 @@ import uk.ac.ebi.atlas.experimentpage.ExperimentAttributesService;
 import uk.ac.ebi.atlas.model.experiment.singlecell.SingleCellBaselineExperiment;
 import uk.ac.ebi.atlas.search.geneids.GeneIdSearchService;
 import uk.ac.ebi.atlas.search.geneids.GeneQuery;
-import uk.ac.ebi.atlas.species.SpeciesFactory;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.utils.StringUtil;
 
-import java.util.*;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.collect.ImmutableList.toImmutableList;

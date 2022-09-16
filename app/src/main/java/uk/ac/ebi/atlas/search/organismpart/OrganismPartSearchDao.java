32e78ab63ad6b3e0fa031ac4f24c802fd1934eb9
Karoly Erdos <karoly@ebi.ac.uk>
2022-09-16 17:53:59 +0100

    Add DAO layer for organism part search by set of gene ids

--- app/src/main/java/uk/ac/ebi/atlas/search/organismpart/OrganismPartSearchDao.java
+++ app/src/main/java/uk/ac/ebi/atlas/search/organismpart/OrganismPartSearchDao.java
@@ -1,14 +1,92 @@
 package uk.ac.ebi.atlas.search.organismpart;
 
 import com.google.common.collect.ImmutableSet;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+import uk.ac.ebi.atlas.search.GeneSearchDao;
+import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
+import uk.ac.ebi.atlas.solr.cloud.TupleStreamer;
+import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy;
+import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
+import uk.ac.ebi.atlas.solr.cloud.search.streamingexpressions.decorator.UniqueStreamBuilder;
+import uk.ac.ebi.atlas.solr.cloud.search.streamingexpressions.source.SearchStreamBuilder;
 
+import java.util.HashSet;
 import java.util.Optional;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.CELL_ID;
+import static uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.CTW_ORGANISM_PART;
 
 @Component
 public class OrganismPartSearchDao {
-    public Optional<ImmutableSet<String>> searchOrganismPart(String queryTerm, String category) {
 
-        return Optional.of(ImmutableSet.of());
+    private GeneSearchDao geneSearchDao;
+
+    private final SingleCellAnalyticsCollectionProxy singleCellAnalyticsCollectionProxy;
+
+    public OrganismPartSearchDao(SolrCloudCollectionProxyFactory collectionProxyFactory,
+                                 GeneSearchDao geneSearchDao) {
+        singleCellAnalyticsCollectionProxy =
+                collectionProxyFactory.create(SingleCellAnalyticsCollectionProxy.class);
+        this.geneSearchDao = geneSearchDao;
+    }
+
+    public Optional<ImmutableSet<String>> searchOrganismPart(ImmutableSet<String> geneIds) {
+        if (geneIds.isEmpty()) {
+            return Optional.of(ImmutableSet.of());
+        }
+
+        HashSet<String> cellIDs = getCellIdsFromGeneIds(geneIds);
+
+        if (cellIDs.isEmpty()) {
+            return Optional.of(ImmutableSet.of());
+        }
+
+//        Streaming query for getting the organism_part provided by set of cell IDs
+//        unique(
+//            search(scxa-analytics-v6, q=cell_id:<SET_OF_CELL_IDS>,
+//            fl="ctw_organism_part",
+//            sort="ctw_organism_part asc"
+//            ),
+//            over="ctw_organism_part"
+//        )
+        var searchAnalyticsQueryBuilder =
+                getAnalyticsQueryBuilderByCellIds(cellIDs);
+        var uniqueOrganismPartStreamBuilder =
+                new UniqueStreamBuilder(searchAnalyticsQueryBuilder, CTW_ORGANISM_PART.name());
+
+        return getOrganismPartFromStreamQuery(uniqueOrganismPartStreamBuilder);
+    }
+
+    @NotNull
+    private HashSet<String> getCellIdsFromGeneIds(ImmutableSet<String> geneIds) {
+        var cellIds = new HashSet<String>();
+        for (var geneId : geneIds) {
+            var cellIdsByGeneID = geneSearchDao.fetchCellIds(geneId);
+            cellIdsByGeneID.values().forEach(cellIds::addAll);
+        }
+        return cellIds;
+    }
+
+    private SearchStreamBuilder<SingleCellAnalyticsCollectionProxy> getAnalyticsQueryBuilderByCellIds(
+            HashSet<String> cellIDs) {
+        return new SearchStreamBuilder<>(
+                singleCellAnalyticsCollectionProxy,
+                new SolrQueryBuilder<SingleCellAnalyticsCollectionProxy>()
+                        .addQueryFieldByTerm(CELL_ID, cellIDs)
+                        .setFieldList(CTW_ORGANISM_PART)
+                        .sortBy(CTW_ORGANISM_PART, SolrQuery.ORDER.asc)
+        ).returnAllDocs();
+    }
+
+    private Optional<ImmutableSet<String>> getOrganismPartFromStreamQuery(UniqueStreamBuilder uniqueOrganismPartStreamBuilder) {
+        try (TupleStreamer tupleStreamer = TupleStreamer.of(uniqueOrganismPartStreamBuilder.build())) {
+            return Optional.of(tupleStreamer.get()
+                    .map(tuple -> tuple.getString(CTW_ORGANISM_PART.name()))
+                    .collect(toImmutableSet())
+            );
+        }
     }
 }
--- app/src/test/java/uk/ac/ebi/atlas/search/organismpart/OrganismPartSearchDaoIT.java
+++ app/src/test/java/uk/ac/ebi/atlas/search/organismpart/OrganismPartSearchDaoIT.java
@@ -1,16 +1,28 @@
 package uk.ac.ebi.atlas.search.organismpart;
 
+import com.google.common.collect.ImmutableSet;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import uk.ac.ebi.atlas.configuration.TestConfig;
+import uk.ac.ebi.atlas.search.GeneSearchDao;
+import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
+import uk.ac.ebi.atlas.testutils.JdbcUtils;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.ac.ebi.atlas.solr.bioentities.BioentityPropertyName.SYMBOL;
 
 @WebAppConfiguration
 @ExtendWith(SpringExtension.class)
@@ -18,21 +30,77 @@ import static uk.ac.ebi.atlas.solr.bioentities.BioentityPropertyName.SYMBOL;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrganismPartSearchDaoIT {
 
+    @Inject
+    private JdbcUtils jdbcUtils;
+
+    @Inject
+    private DataSource dataSource;
+
+    @Inject
+    private SolrCloudCollectionProxyFactory collectionProxyFactory;
+
+    @Inject
+    private GeneSearchDao geneSearchDao;
+
     private OrganismPartSearchDao subject;
+
+    @BeforeAll
+    void populateDatabaseTables() {
+        var populator = new ResourceDatabasePopulator();
+        populator.addScripts(
+                new ClassPathResource("fixtures/scxa_analytics.sql"),
+                new ClassPathResource("fixtures/experiment.sql")
+        );
+
+        populator.execute(dataSource);
+    }
+
+    @AfterAll
+    void cleanDatabaseTables() {
+        var populator = new ResourceDatabasePopulator();
+        populator.addScripts(
+                new ClassPathResource("fixtures/experiment-delete.sql"),
+                new ClassPathResource("fixtures/scxa_analytics-delete.sql")
+        );
+        populator.execute(dataSource);
+    }
 
     @BeforeEach
     void setup() {
-        subject = new OrganismPartSearchDao();
+        subject = new OrganismPartSearchDao(collectionProxyFactory, geneSearchDao);
     }
 
     @Test
-    void whenEmptySearchTextProvidedReturnEmptyOptional() {
-        var queryTerm = "";
-        var category = SYMBOL.name;
+    void whenEmptySetOfGeneIDsProvidedReturnEmptyOptional() {
+        ImmutableSet<String> geneIds = ImmutableSet.of();
 
-        var organismPart = subject.searchOrganismPart(queryTerm, category);
+        var organismParts = subject.searchOrganismPart(geneIds);
 
-        assertThat(organismPart.isPresent()).isTrue();
-        assertThat(organismPart.get()).isEmpty();
+        assertThat(organismParts.isPresent()).isTrue();
+        assertThat(organismParts.get()).isEmpty();
+    }
+
+    @Test
+    void whenSetOfInvalidGeneIdsProvidedReturnSetOfOrganismPart() {
+        ImmutableSet<String> geneIds =
+                ImmutableSet.of("invalid-geneId-1", "invalid-geneId-2", "invalid-geneId-3");
+
+        var organismParts = subject.searchOrganismPart(geneIds);
+
+        assertThat(organismParts.isPresent()).isTrue();
+        assertThat(organismParts.get()).isEmpty();
+    }
+
+    @Test
+    void whenSetOfValidGeneIdsProvidedReturnSetOfOrganismPart() {
+        final List<String> randomListOfGeneIds = jdbcUtils.fetchRandomListOfGeneIds(10);
+        ImmutableSet<String> geneIds =
+                ImmutableSet.copyOf(
+                        new HashSet<String>(randomListOfGeneIds));
+
+        var organismParts = subject.searchOrganismPart(geneIds);
+
+        assertThat(organismParts.isPresent()).isTrue();
+        assertThat(organismParts.get().size()).isGreaterThan(0);
     }
 }

263888e3f3b19351c6b43cf656b9fad81a42dfa6
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-11-17 09:55:59 +0000

    Fix experiment design volume name and destination directory

--- docker/prepare-dev-environment/volumes/run.sh
+++ docker/prepare-dev-environment/volumes/run.sh
@@ -22,7 +22,7 @@ function print_usage() {
   printf '%b\n' " • ${ATLAS_DATA_BIOENTITY_PROPERTIES_VOL_NAME}: gene annotations from array designs, Ensembl, Reactome,"
   printf '%b\n' "                                         WormBase ParaSite, miRBase, Gene Ontology and InterPro"
   printf '%b\n' " • ${ATLAS_DATA_SCXA_VOL_NAME}: test experiments data bundles, species and release definition files"
-  printf '%b\n' " • ${ATLAS_DATA_EXPDESIGN_VOL_NAME}: experiment design files of test experiments"
+  printf '%b\n' " • ${ATLAS_DATA_SCXA_EXPDESIGN_VOL_NAME}: experiment design files of test experiments"
   printf '%b\n' " • ${WEBAPP_PROPERTIES_VOL_NAME}: configuration files for the web application"
   printf '\n%b\n' "-r\tRemove volumes before creating them"
   printf '\n%b\n' "-l FILE\tLog file (default is /dev/stdout)"
@@ -52,7 +52,7 @@ do
   esac
 done
 
-ALL_VOLUMES="${ATLAS_DATA_BIOENTITY_PROPERTIES_VOL_NAME} ${ATLAS_DATA_SCXA_VOL_NAME} ${ATLAS_DATA_EXPDESIGN_VOL_NAME} ${WEBAPP_PROPERTIES_VOL_NAME}"
+ALL_VOLUMES="${ATLAS_DATA_BIOENTITY_PROPERTIES_VOL_NAME} ${ATLAS_DATA_SCXA_VOL_NAME} ${ATLAS_DATA_SCXA_EXPDESIGN_VOL_NAME} ${WEBAPP_PROPERTIES_VOL_NAME}"
 if [ "${REMOVE_VOLUMES}" = "true" ]; then
   print_stage_name "🗑 Remove Docker volumes: ${ALL_VOLUMES}"
   for VOL_NAME in ${ALL_VOLUMES}
@@ -75,7 +75,7 @@ docker build \
 --build-arg ATLAS_DATA_BIOENTITY_PROPERTIES_DEST=${ATLAS_DATA_BIOENTITY_PROPERTIES_DEST} \
 --build-arg ATLAS_DATA_SCXA_DEST=${ATLAS_DATA_SCXA_DEST} \
 --build-arg WEBAPP_PROPERTIES_DEST=${WEBAPP_PROPERTIES_DEST} \
---build-arg ATLAS_DATA_EXPDESIGN_DEST=${ATLAS_DATA_EXPDESIGN_DEST} \
+--build-arg ATLAS_DATA_SCXA_EXPDESIGN_DEST=${ATLAS_DATA_SCXA_EXPDESIGN_DEST} \
 --build-arg EXP_IDS="${EXP_IDS}" \
 -t ${IMAGE_NAME} . &>> ${LOG_FILE}
 print_done
@@ -89,11 +89,11 @@ ${IMAGE_NAME} &>> ${LOG_FILE}
 print_done
 
 printf '%b\n' "🙂 All done! You can inspect the volume contents attaching them to a container:"
-printf '%b\n' "   # ${ATLAS_DATA_EXPDESIGN_VOL_NAME} will be empty until we load data in Postgres"
+printf '%b\n' "   # ${ATLAS_DATA_SCXA_EXPDESIGN_VOL_NAME} will be empty until we load data in Postgres"
 printf '%b\n' "   docker run \\"
 printf '%b\n' "   -v ${ATLAS_DATA_BIOENTITY_PROPERTIES_VOL_NAME}:${ATLAS_DATA_BIOENTITY_PROPERTIES_DEST} \\"
 printf '%b\n' "   -v ${ATLAS_DATA_SCXA_VOL_NAME}:${ATLAS_DATA_SCXA_DEST} \\"
-printf '%b\n' "   -v ${ATLAS_DATA_EXPDESIGN_VOL_NAME}:${ATLAS_DATA_EXPDESIGN_DEST} \\"
+printf '%b\n' "   -v ${ATLAS_DATA_SCXA_EXPDESIGN_VOL_NAME}:${ATLAS_DATA_SCXA_EXPDESIGN_DEST} \\"
 printf '%b\n' "   -v ${WEBAPP_PROPERTIES_VOL_NAME}:${WEBAPP_PROPERTIES_DEST} \\"
 printf '%b\n' "   --rm -it ubuntu:jammy bash"
 

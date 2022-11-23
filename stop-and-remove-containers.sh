18270065ef520b3a006c2a9d324c12a440f67746
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-11-23 14:13:50 +0000

    Fix containers names in stop-and-remove-containers.sh

--- stop-and-remove-containers.sh
+++ stop-and-remove-containers.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+docker stop \
+scxa-solrcloud-0 scxa-solrcloud-1 \
+scxa-solrcloud-zookeeper-0 scxa-solrcloud-zookeeper-1 scxa-solrcloud-zookeeper-2 \
+scxa-postgres-test scxa-flyway-test \
+scxa-gradle
 
-docker stop scxa-solrcloud-1 scxa-solrcloud-2 scxa-zk-1 scxa-zk-2 scxa-zk-3 scxa-postgres-test scxa-flyway-test scxa-gradle
-
-docker rm scxa-solrcloud-1 scxa-solrcloud-2 scxa-zk-1 scxa-zk-2 scxa-zk-3 scxa-postgres-test scxa-flyway-test scxa-gradle
+docker rm \
+scxa-solrcloud-0 scxa-solrcloud-1 \
+scxa-solrcloud-zookeeper-0 scxa-solrcloud-zookeeper-1 scxa-solrcloud-zookeeper-2 \
+scxa-postgres-test scxa-flyway-test \
+scxa-gradle

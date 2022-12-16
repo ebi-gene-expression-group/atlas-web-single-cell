0713d7965e8e6cdef32ec8e210d40886487b1fb4
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-12-16 18:20:56 +0000

    Add request parameter to JSON controller and add  a fix/add tests

--- app/src/main/java/uk/ac/ebi/atlas/search/metadata/JsonCellTypeWheelController.java
+++ app/src/main/java/uk/ac/ebi/atlas/search/metadata/JsonCellTypeWheelController.java
@@ -3,9 +3,11 @@ package uk.ac.ebi.atlas.search.metadata;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
@@ -21,9 +23,13 @@ public class JsonCellTypeWheelController extends JsonExceptionHandlingController
 
     @GetMapping(value = "/json/cell-type-wheel/{searchTerm}",
                 produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String jsonCellTypeWheel(@PathVariable String searchTerm) {
+    public String jsonCellTypeWheel(@PathVariable String searchTerm,
+                                    @RequestParam(name = "species", defaultValue = "") String species) {
         return GSON.toJson(
                 highchartsSunburstAdapter.getCellTypeWheelSunburst(
-                        searchTerm, cellTypeWheelService.search(searchTerm)));
+                        searchTerm,
+                        isBlank(species) ?
+                                cellTypeWheelService.search(searchTerm) :
+                                cellTypeWheelService.search(searchTerm, species)));
     }
 }

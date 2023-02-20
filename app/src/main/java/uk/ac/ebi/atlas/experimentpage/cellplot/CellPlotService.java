1d4bd23826a4ee697c2c95fe7050aad05d9466c5
upendrakumbham <upendra.kumbham@gmail.com>
2023-02-20 11:23:29 +0000

    Feature/181713000 pick the default plot type and parameter values from the backend (#272)
    * Add a sql query and DAO logic to fetch default plot type and it's plot options
    
    * Add ann data fixtures
    
    * Improve cellplotdao method()
    
    * Add a method in the service and serializer class to fetch default plot type and it's option from the DAO
    
    * Add an endpoint method to fetch JSON payload of default plot type and option
    
    * Update SQL query and it's dao method
    
    * Add a test for the default plottype with option fetching functionality from the DB
    
    * Add a test for the  empty default plot type scenario test
    
    * Update return type
    
    * Add service class tests
    
    * Update endpoint for the default plot type
    
    * Improve tests
    
    * Bump atlas-web-core
    
    * Rename endpoint from plot type to plot method
    
    * refactor method names insync with DB column names
    
    * Refactor tests method signature insync with DB column names
    
    * Add a white space after comma
    
    * Remove redundant line
    
    * Refactor plot type to plot method
    
    * Update endpoint
    
    * Update test methods
    
    * Bump atlas-web-core
    
    * Added V19 flyway script to make it availble scxa_dimension_reduction table
    
    * Removed Optional.ofNullable() redundant code as it is giving unwanted extra wrapper object for the JSON output
    
    * Change order of the script loading as we are using scxa reduction table ID in the coords table as a foreign key
    
    * Updates scxa coords table data as per V19 schema
    
    * Add default method plot and parameterisation to...
    
    experiment page result content
    
    * Fix broken contructor
    
    * Add scxa dimension reduction table script to populater to load data and delete data
    
    * Replace ImmutableMap value from String to JsonObject
    
    * Replace ImmutableMap value from String to JsonObject
    
    * Fix broken tests after replacing ImmutableMap value String with JsonObject
    
    * Fix contructor dependency and add a new test for the default plot type feature
    
    * Fix contructor dependency injection
    
    * Add a method to extract result from the CellPlotService and add it to the result jsonarray
    
    * Inject defaultplotmethod payload as part of metadata json payload to make availble to third party users
    
    * Add tests to the newly extracted method for default plot method
    
    * Bump atlas-web-core
    
    * Bump schemas submodule
    
    * Fix broken test
    
    * Fix broken test
    
    * Removed conditional check
    
    * Update code to return Map<key,List<values>> as we changed SQL query
    
    * Find Middle element from the options List for the umap, tsne method types and fill the default MAP
    
    * Rename variables
    
    * Increase HashMap access scope
    
    * Add if check dao result not empty
    
    * Fix broken tests
    
    * Bump atlas-web-core
    
    * Added experimentDesignLocation attribute and it's value
    
    * Rename keys as per DB like 'tsne' and 'umap'
    
    * Remove reduntant variables and rename variable to improve readability
    
    ---------
    
    Co-authored-by: lingyun1010 <zhaolingyun1010@gmail.com>

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/cellplot/CellPlotService.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/cellplot/CellPlotService.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.atlas.experimentpage.cellplot;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.gson.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math.util.MathUtils;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.function.Function.identity;
@@ -100,4 +104,29 @@ public class CellPlotService {
                                         pointDto.name()))
                 .collect(toImmutableSet());
     }
+
+    public ImmutableMap<String, JsonObject> fetchDefaultPlotMethodWithParameterisation(String experimentAccession) {
+        var umap = "umap";
+        var tsne = "tsne";
+        var defaultcellPlots = cellPlotDao.fetchDefaultPlotMethodWithParameterisation(experimentAccession);
+
+        ImmutableMap.Builder<String, JsonObject> defaultPlotTypeAndOptions = new ImmutableMap.Builder<>();
+        if (!defaultcellPlots.isEmpty()) {
+            defaultPlotTypeAndOptions.put(umap, getMiddleElement(defaultcellPlots.get(umap)));
+            defaultPlotTypeAndOptions.put(tsne, getMiddleElement(defaultcellPlots.get(tsne)));
+        }
+        return defaultPlotTypeAndOptions.build();
+    }
+
+    private static JsonObject getMiddleElement(List plotOptions) {
+
+        if (plotOptions.size() % 2 == 0) { // even number
+            Object umapEvenItem = plotOptions.get((plotOptions.size() / 2 - 1));
+            return (JsonObject) umapEvenItem;
+        } else { //odd number
+            Object umapOddItem = plotOptions.get((plotOptions.size() / 2));
+            return (JsonObject) umapOddItem;
+        }
+    }
+
 }

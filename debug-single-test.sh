64f8b13a49b9c531a9ea1cad651186efcb91345e
Karoly Erdos <karoly@ebi.ac.uk>
2023-03-03 14:32:20 +0000

    Improve docker setup and supporting scripts

--- debug-single-test.sh
+++ debug-single-test.sh
@@ -4,10 +4,56 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source ${SCRIPT_DIR}/docker/dev.env
 
-TEST_CASE_NAME=$1
-echo "Testing ${TEST_CASE_NAME}"
+function print_usage() {
+  printf '\n%b\n\n' "Usage: ${0} [ -a APPLICATION_NAME ] [ -s SCHEMA_VERSION ]"
+  printf '%b\n' "Debugging a given unit/integration test from the given application using the given schema version"
+  printf '\n%b\n' "-p\tName of the project the test can be found\tfor example: app or atlas-web-core"
+  printf '\n%b\n' "-s\tNumeric version of the schema or latest\tfor example: 18 (default is latest)"
+  printf '%b\n\n' "-h\tShow usage instructions"
+}
 
-SCHEMA_VERSION=${2:-latest} \
+PROJECT_NAME=app
+SCHEMA_VERSION=latest
+mandatory_name=false
+
+while getopts "n:p:s:h" opt
+do
+  case ${opt} in
+    n )
+      mandatory_name=true; TEST_CASE_NAME=${OPTARG}
+      ;;
+    p )
+      PROJECT_NAME=${OPTARG}
+      if ! [[ "$PROJECT_NAME" =~ ^(app|atlas-web-core)$ ]]; then
+        echo "Project name is not valid: $OPTARG" >&2
+        exit 1
+      fi
+      ;;
+    s )
+      SCHEMA_VERSION=${OPTARG}
+      ;;
+    h )
+      print_usage
+      exit 0
+      ;;
+    \?)
+      printf '%b\n' "Invalid option: -${OPTARG}" >&2
+      print_usage
+      exit 2
+      ;;
+    : ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+  esac
+done
+
+if ! $mandatory_name
+then
+    echo "-n must be provided with the name of the test to execute" >&2
+    exit 1
+fi
+
+echo "Debugging ${TEST_CASE_NAME}"
+
+SCHEMA_VERSION={SCHEMA_VERSION} \
 docker-compose \
 --env-file ${SCRIPT_DIR}/docker/dev.env \
 -f docker/docker-compose-postgres-test.yml \
@@ -17,9 +63,9 @@ run --rm --service-ports \
 scxa-gradle bash -c "
 set -e
 
-./gradlew clean
+gradle clean
 
-./gradlew \
+gradle \
 -PdataFilesLocation=/atlas-data \
 -PexperimentFilesLocation=/atlas-data/scxa \
 -PexperimentDesignLocation=/atlas-data/expdesign \
@@ -28,7 +74,7 @@ set -e
 -PjdbcPassword=${POSTGRES_PASSWORD} \
 -PzkHosts=${SOLR_CLOUD_ZK_CONTAINER_1_NAME}:2181,${SOLR_CLOUD_ZK_CONTAINER_2_NAME}:2181,${SOLR_CLOUD_ZK_CONTAINER_3_NAME}:2181 \
 -PsolrHosts=http://${SOLR_CLOUD_CONTAINER_1_NAME}:8983/solr,http://${SOLR_CLOUD_CONTAINER_2_NAME}:8983/solr \
-app:testClasses
+${PROJECT_NAME}:testClasses
 
-./gradlew --continuous -PremoteDebug :app:test --tests $TEST_CASE_NAME
+gradle --continuous -PremoteDebug :${PROJECT_NAME}:test --tests $TEST_CASE_NAME
 "

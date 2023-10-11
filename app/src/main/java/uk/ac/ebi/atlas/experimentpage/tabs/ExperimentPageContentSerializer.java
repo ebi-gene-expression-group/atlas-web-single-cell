a7cd01093ec8a6f6f2e88aa07acad2f0771be046
lingyun1010 <zhaolingyun1010@gmail.com>
2023-10-11 09:13:30 +0100

    Add experiment design payload with DAO search

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/tabs/ExperimentPageContentSerializer.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/tabs/ExperimentPageContentSerializer.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.model.experiment.ExperimentDesignTable;
+import uk.ac.ebi.atlas.trader.ExperimentDesignDao;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
@@ -12,12 +13,15 @@ import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 @Component
 public class ExperimentPageContentSerializer {
     private final ExperimentTrader experimentTrader;
+    private final ExperimentDesignDao experimentDesignDao;
     private final ExperimentPageContentService experimentPageContentService;
 
     public ExperimentPageContentSerializer(ExperimentTrader experimentTrader,
+                                           ExperimentDesignDao experimentDesignDao,
                                            ExperimentPageContentService experimentPageContentService) {
         this.experimentTrader = experimentTrader;
         this.experimentPageContentService = experimentPageContentService;
+        this.experimentDesignDao = experimentDesignDao;
     }
 
     @Cacheable(cacheNames = "jsonExperimentPageTabs", key = "#experimentAccession")
@@ -33,8 +37,11 @@ public class ExperimentPageContentSerializer {
         var availableTabs = new JsonArray();
 
         var results = experimentPageContentService.getTsnePlotData(experiment.getAccession());
+        var experimentDesignJson =
+                experimentPageContentService.getExperimentDesign(
+                        experiment.getAccession(), new ExperimentDesignTable(experiment, experimentDesignDao).asJson(), accessKey);
         availableTabs.add(customContentTab("results", "Results", results));
-        availableTabs.add(customContentTab("experiment-design", "Experiment Design", new JsonObject()));
+        availableTabs.add(customContentTab("experiment-design", "Experiment Design", experimentDesignJson));
 
         var sections = new JsonObject();
         sections.add("sections", experimentPageContentService.getSupplementaryInformation(experiment.getAccession()));

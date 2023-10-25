2885362021d2fabdfae49f8b30e0098d7223754a
lingyun1010 <zhaolingyun1010@gmail.com>
2023-10-25 11:37:02 +0100

    Update tsne-plot package

--- app/src/main/javascript/bundles/experiment-page/src/TSnePlotViewRoute.js
+++ app/src/main/javascript/bundles/experiment-page/src/TSnePlotViewRoute.js
@@ -42,8 +42,8 @@ class TSnePlotViewRoute extends React.Component {
       selectedColourBy: cellTypeValue ? cellTypeValue.toLowerCase() : this.props.ks[Math.round((this.props.ks.length -1) / 2)].toString(),
       highlightClusters: [],
       experimentAccession: this.props.experimentAccession,
-      selectedColourByCategory: !isNaN(search.colourBy) ? `clusters` : `metadata`,
-      selectedClusterByCategory: !isNaN(search.colourBy) ? `clusters` : `metadata`,
+      selectedColourByCategory: isNaN(search.colourBy) ? `metadata` :  `clusters`,
+      //selectedClusterByCategory: !isNaN(search.colourBy) ? `clusters` : `metadata`,
       selectedClusterId: cellTypeValue ? cellTypeValue.toLowerCase() : this.props.ks[Math.round((this.props.ks.length -1) / 2)].toString(),
       selectedClusterIdOption: ``
     }

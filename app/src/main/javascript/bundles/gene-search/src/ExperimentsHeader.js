27edce8c379520556bbd67d5ce1fc3e5da46919c
Lingyun Zhao <33519183+lingyun1010@users.noreply.github.com>
2024-05-24 18:13:08 +0100

    Integrate scxa-faceted-search-result to move the anndata icon to the title column (#405)
    * Update scxa-faceted-search-results package version
    
    * Update experiment card and header as front-end

--- app/src/main/javascript/bundles/gene-search/src/ExperimentsHeader.js
+++ app/src/main/javascript/bundles/gene-search/src/ExperimentsHeader.js
@@ -1,59 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 import PropTypes from "prop-types"
+import {
+  IconDiv,
+  MarkerDiv,
+  TitleDiv,
+  VariableDiv,
+  CountDiv,
+  CardContainerDiv
+} from './ExperimentsHeaderDiv'
 
-const CardContainerDiv = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex !important;
-  flex-wrap: nowrap;
-  align-items: center;
-  padding: 1rem;
-  font-weight: bolder;
-  font-size: 0.9rem;
-`
-
-const IconDiv = styled.div`
-  width: 15%;
-  text-align: center;
-  cursor: pointer;
-  opacity: 0.6;
-  transition: 0.3s;
-  :hover {opacity: 1};
-`
-
-const MarkerDiv = styled.div`
-  width: 15%;
-  text-align: center;
-  cursor: pointer;
-  opacity: 0.6;
-  transition: 0.3s;
-  :hover {opacity: 1};
-`
-
-const TitleDiv = styled.div`
-  width: 40%;
-  text-align: center;
-  cursor: pointer;
-  opacity: 0.6;
-  transition: 0.3s;
-  :hover {opacity: 1};
-`
-
-const VariableDiv = styled.div`
-  width: 20%;
-  text-align: center;
-  opacity: 0.6;
-`
-
-const CountDiv = styled.div`
-  width: 10%;
-  text-align: center;
-  cursor: pointer;
-  opacity: 0.6;
-  transition: 0.3s;
-  :hover {opacity: 1};
-`
 
 const ExperimentTableHeaderBasic = () =>
   ({

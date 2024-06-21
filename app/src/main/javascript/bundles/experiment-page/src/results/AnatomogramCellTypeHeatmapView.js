8c0ae9dfee859c58443e495ffa803b59e19727d9
Lingyun Zhao <33519183+lingyun1010@users.noreply.github.com>
2024-06-21 09:53:17 +0100

    Integrate ovary and testis organ anatomogram (#417)
    * Add ovary and tesits(reproduction) into organ-ana
    
    tomogram view
    
    * Update organ-anatomogram package version
    
    * Refactor species and view into switch condition
    
    * Import ontology ids from json
    
    * Add anatomogram props for demo
    
    * Add ovary and reproduction into supported organ

--- app/src/main/javascript/bundles/experiment-page/src/results/AnatomogramCellTypeHeatmapView.js
+++ app/src/main/javascript/bundles/experiment-page/src/results/AnatomogramCellTypeHeatmapView.js
@@ -4,15 +4,7 @@ import URI from 'urijs'
 
 import Anatomogram from '@ebi-gene-expression-group/organ-anatomogram'
 import HeatmapView from '@ebi-gene-expression-group/scxa-marker-gene-heatmap'
-
-const supportedOrgans = {
-  pancreas: `UBERON_0001264`,
-  kidney: `UBERON_0002113`,
-  liver: `UBERON_0002107`,
-  placenta: `UBERON_0001987`,
-  lung: `UBERON_0002048`,
-  gut: `UBERON_0000160`
-}
+import ontologyIds from './ontologyIds.json'
 
 const ontologyAccessionToOntologyUri = (accession) => {
   return accession.startsWith(`EFO`) ?
@@ -30,9 +22,9 @@ class AnatomogramCellTypeHeatmapView extends React.Component {
       selectIds: [],
       selectAllIds: [],
       resource:
-        URI(`json/experiments/${props.experimentAccession}/marker-genes/cell-types`, props.host)
-          .search({organismPart: ontologyAccessionToOntologyUri(supportedOrgans[props.organ])})
-          .toString()
+          URI(`json/experiments/${props.experimentAccession}/marker-genes/cell-types`, props.host)
+              .search({organismPart: ontologyAccessionToOntologyUri(ontologyIds[props.organ][props.organ][0])})
+              .toString()
     }
 
     this._addRemoveFromSelectIds = this._addRemoveFromSelectIds.bind(this)
@@ -56,87 +48,21 @@ class AnatomogramCellTypeHeatmapView extends React.Component {
       this.setState({
         selectIds: ids,
         resource:
-          URI(`json/experiments/${this.props.experimentAccession}/marker-genes/cell-types`, this.props.host)
-            .search({organismPart: ids.map(ontologyAccessionToOntologyUri).join(`,`)})
-            .toString()
+            URI(`json/experiments/${this.props.experimentAccession}/marker-genes/cell-types`, this.props.host)
+                .search({organismPart: ids.map(ontologyAccessionToOntologyUri).join(`,`)})
+                .toString()
       })
     }
   }
 
   _afterSwitchView(species, view) {
     let requestOntologyIds = []
-    if (species === `kidney`) {
-      if (view === `kidney`) {
-        requestOntologyIds = [supportedOrgans[species]]
-      } else if (view === `nephron`) {
-        requestOntologyIds = [`UBERON_0001285`]
-      } else if (view === `glomerulus`) {
-        requestOntologyIds = [`UBERON_0000074`]
-      } else if (view === `duct`) {
-        requestOntologyIds = [`UBERON_0001232`]
-      }
-    } else if (species === `liver`) {
-      if (view === `liver`) {
-        requestOntologyIds = [supportedOrgans[species]]
-      } else if (view === `lobule`) {
-        requestOntologyIds = [ // `UBERON_0004647`,
-          `CL_0000182`, `CL_0000632`, `UBERON_0001282`, `CL_1000488`, `UBERON_0001193`, `UBERON_0006841`,
-          `UBERON_0001639`, `EFO_0010704`, `UBERON_0001281`, `CL_1000398`, `EFO_0010705`, `EFO_0010706`, `CL_0000091`
-        ]
-      }
-    } else if (species === `lung`) {
-      if (view === `lung`) {
-        requestOntologyIds = [supportedOrgans[species]]
-      } else if (view === `airway`) {
-        requestOntologyIds = [ // `UBERON_0002184`,
-          `CL_0002633`, `CL_1001567`,  `CL_1000271`, `CL_0000158`, `EFO_0010666`, `CL_0002370`, `UBERON_0003504`,
-          `CL_0002598`
-        ]
-      } else if (view === `alveoli_fin`) {
-        requestOntologyIds = [ `UBERON_0002299`, `UBERON_0002188`, `UBERON_0016405` ]
-      } else if (view === `alveoli_section`) {
-        requestOntologyIds = [ // `UBERON_0003215`,
-          `CL_0002062`, `CL_0002063`, `UBERON_0003456`, `EFO_0010667`, `EFO_0010668`, `EFO_0010669`
-        ]
-      }
-    } else if (species === `pancreas`) {
-      if (view === `pancreas`) {
-        requestOntologyIds = [supportedOrgans[species]]
-      } else if (view === `acinus`) {
-        requestOntologyIds = [
-          `CL_0000173`, `CL_0000171`, `CL_0000169`, `CL_0002275`, `CL_0005019`, `CL_0002410`, `CL_0000622`
-        ]
-      }
-    } else if (species === `placenta`) {
-      if (view === `placenta`) {
-        requestOntologyIds = [supportedOrgans[species]]
-      } else if (view === `cells`) {
-        requestOntologyIds = [
-          `CL_2000002`, `EFO_0010710`, `CL_3000001`, `UBERON_0000371`, `UBERON_0000319`, `UBERON_0000426`,
-          `EFO_0010712`, `EFO_0010711`, `CL_0002601`, `EFO_0010708`
-        ]
-      }
-    } else if (species === `gut`) {
-      if (view === `gut`) {
-        requestOntologyIds = [supportedOrgans[species]]
-      } else if (view === `colon`) {
-        requestOntologyIds = [
-          "UBERON_8410051", "UBERON_8410015", "UBERON_8410048", "UBERON_8410057",
-          "UBERON_0007178", "UBERON_8410000", "UBERON_8410061", "UBERON_8410058", "UBERON_8410059",
-          "CL_0009043", "CL_0009042", "CL_0009041", "CL_1000347", "CL_0009039", "CL_0009011",
-          "CL_0009009", "CL_0009025", "CL_0009038", "CL_0009040", "UBERON_8410060"
-        ]
-      } else if (view === `intestine`) {
-        requestOntologyIds = [
-          "CL_1000275", "CL_1000334", "UBERON_8410002", "UBERON_0018410", "UBERON_8410004",
-          "UBERON_8410001", "UBERON_8410063", "UBERON_8410064", "UBERON_0001210", "UBERON_0012401",
-          "UBERON_0012402", "CL_0009012", "CL_1000495", "CL_1000343", "CL_1000353", "CL_0009080",
-          "CL_0009017", "CL_0009006", "CL_0009024", "CL_0009022", "CL_1000411", "CL_0009015", "CL_0009007", "UBERON_8410068"
-        ]
-      }
+
+    if (ontologyIds[species] && ontologyIds[species][view]) {
+      requestOntologyIds = ontologyIds[species][view]
     }
 
-    this.setState({
+  this.setState({
        resource:
          URI(`json/experiments/${this.props.experimentAccession}/marker-genes/cell-types`, this.props.host)
            .search({ organismPart: requestOntologyIds.map(ontologyAccessionToOntologyUri).join(`,`) })

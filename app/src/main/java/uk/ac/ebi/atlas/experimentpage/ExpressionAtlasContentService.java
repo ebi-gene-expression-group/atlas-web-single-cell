817601726311649baca2ab51a5088981726bfd8f
lingyun zhao <zhaolingyun1010@gmail.com>
2024-06-21 10:47:40 +0100

    Decouple the experiment type with external links

--- app/src/main/java/uk/ac/ebi/atlas/experimentpage/ExpressionAtlasContentService.java
+++ app/src/main/java/uk/ac/ebi/atlas/experimentpage/ExpressionAtlasContentService.java
@@ -2,82 +2,88 @@ package uk.ac.ebi.atlas.experimentpage;
 
 import com.google.common.collect.ImmutableList;
 import org.springframework.stereotype.Component;
-import uk.ac.ebi.atlas.experimentpage.link.LinkToArrayExpress;
-import uk.ac.ebi.atlas.experimentpage.link.LinkToEga;
-import uk.ac.ebi.atlas.experimentpage.link.LinkToEna;
-import uk.ac.ebi.atlas.experimentpage.link.LinkToGeo;
+import uk.ac.ebi.atlas.experimentpage.link.*;
 import uk.ac.ebi.atlas.model.download.ExternallyAvailableContent;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.singlecell.SingleCellBaselineExperiment;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static com.google.common.collect.ImmutableList.toImmutableList;
 
 @Component
 public class ExpressionAtlasContentService {
 
-    private final ExternallyAvailableContentService<SingleCellBaselineExperiment>
-            singleCellBaselineExperimentExternallyAvailableContentService;
-    private final ExternallyAvailableContentService<SingleCellBaselineExperiment>
-            singleCellBaselineExperimentExternallyAvailableContentServiceGeo;
-    private final ExternallyAvailableContentService<SingleCellBaselineExperiment>
-            singleCellBaselineExperimentExternallyAvailableContentServiceEna;
-    private final ExternallyAvailableContentService<SingleCellBaselineExperiment>
-            singleCellBaselineExperimentExternallyAvailableContentServiceEga;
+    private final LinkToEna linkToEna;
+    private final LinkToEga linkToEga;
+    private final LinkToGeo linkToGeo;
+    private final LinkToArrayExpress linkToArrayExpress;
     private final ExperimentTrader experimentTrader;
 
     public ExpressionAtlasContentService(
-            LinkToArrayExpress.SingleCell singleCellLinkToArrayExpress
-            ,
-            LinkToEna.SingleCell singleCellLinkToEna,
-            LinkToEga.SingleCell singleCellLinkToEga,
-            LinkToGeo.SingleCell singleCellLinkToGeo,
+            LinkToArrayExpress linkToArrayExpress,
+            LinkToEna linkToEna,
+            LinkToEga linkToEga,
+            LinkToGeo linkToGeo,
             ExperimentTrader experimentTrader) {
         this.experimentTrader = experimentTrader;
-
-        this.singleCellBaselineExperimentExternallyAvailableContentService=
-                new ExternallyAvailableContentService<>(ImmutableList.of(singleCellLinkToArrayExpress));
-
-        this.singleCellBaselineExperimentExternallyAvailableContentServiceGeo =
-                new ExternallyAvailableContentService<>(ImmutableList.of(singleCellLinkToGeo));
-
-        this.singleCellBaselineExperimentExternallyAvailableContentServiceEna =
-                new ExternallyAvailableContentService<>(ImmutableList.of(singleCellLinkToEna));
-
-        this.singleCellBaselineExperimentExternallyAvailableContentServiceEga =
-                new ExternallyAvailableContentService<>(ImmutableList.of(singleCellLinkToEga));
+        this.linkToEna = linkToEna;
+        this.linkToEga = linkToEga;
+        this.linkToGeo = linkToGeo;
+        this.linkToArrayExpress = linkToArrayExpress;
     }
 
     public List<ExternallyAvailableContent> getExternalResourceLinks(String experimentAccession,
                                                  String accessKey,
                                                  ExternallyAvailableContent.ContentType contentType) {
         Experiment<?> experiment = experimentTrader.getExperiment(experimentAccession, accessKey);
-        var externalResourceType = getExternalResouceType(experiment);
 
-        var arrayExpressResourceLinks = singleCellBaselineExperimentExternallyAvailableContentService.list(
-                (SingleCellBaselineExperiment) experiment, contentType);
-        var otherExternalResourceLinks =  externalResourceType.equals("geo") ?
-                singleCellBaselineExperimentExternallyAvailableContentServiceGeo.list(
-                (SingleCellBaselineExperiment) experiment, contentType) :
-                externalResourceType.equals("ega") ?
-                        singleCellBaselineExperimentExternallyAvailableContentServiceEga.list(
-                        (SingleCellBaselineExperiment) experiment, contentType) :
-                        singleCellBaselineExperimentExternallyAvailableContentServiceEna.list(
-                        (SingleCellBaselineExperiment) experiment, contentType);
-        otherExternalResourceLinks.addAll(arrayExpressResourceLinks);
-        return otherExternalResourceLinks;
+        ImmutableList.Builder<ExternallyAvailableContent> arrayExpressResourceLinks = ImmutableList.builder();
+
+        if (experimentAccession.matches("E-MTAB.*|E-ERAD.*|E-GEUV.*")) {
+            arrayExpressResourceLinks.addAll(linkToArrayExpress.get(experiment));
+        }
+        var otherExternalResourceLinks =  externalResourceLinks(experiment);
+        arrayExpressResourceLinks.addAll(otherExternalResourceLinks.build());
+
+        return arrayExpressResourceLinks.build();
     }
 
-    private String getExternalResouceType(Experiment<?> experiment) {
-        var geoAccessions = experiment.getSecondaryAccessions().stream()
-                .filter(accession -> accession.matches("GSE.*"))
-                .collect(toImmutableList());
-        var egaAccessions = experiment.getSecondaryAccessions().stream()
-                .filter(accession -> accession.matches("EGA.*"))
-                .collect(toImmutableList());
+    private ImmutableList.Builder<ExternallyAvailableContent> externalResourceLinks(Experiment<?> experiment) {
+        ImmutableList.Builder<ExternallyAvailableContent> otherExternalResourceLinks = ImmutableList.builder();
 
-        return geoAccessions.isEmpty() ? egaAccessions.isEmpty() ? "ena" : "ega" : "geo";
+        Map<String, List<String>> resourceList = experiment.getSecondaryAccessions().stream()
+                .collect(Collectors.groupingBy(accession -> {
+                    if (accession.matches("GSE.*")) return "GEO";
+                    if (accession.matches("EGA.*")) return "EGA";
+                    if (accession.matches("PDX.*")) return "PRIDE";
+                    if (accession.matches("ERP.*|SRP.*|DRP.*|PRJEB.*|PRJNA.*|PRJDB.*")) return "ENA";
+                    return "OTHER";
+                }));
 
+        resourceList.replaceAll((k, v) -> v.stream().collect(toImmutableList()));
+
+        // Remove "OTHER" key if it exists
+        resourceList.remove("OTHER");
+
+        // Add to the builder based on the resource type
+        resourceList.forEach((resource, accessions) -> {
+            switch (resource) {
+                case "GEO":
+                    otherExternalResourceLinks.addAll(linkToGeo.get(experiment));
+                    break;
+                case "EGA":
+                    otherExternalResourceLinks.addAll(linkToEga.get(experiment));
+                    break;
+                case "ENA":
+                    otherExternalResourceLinks.addAll(linkToEna.get(experiment));
+                    break;
+            }
+        });
+
+        return otherExternalResourceLinks;
     }
 }

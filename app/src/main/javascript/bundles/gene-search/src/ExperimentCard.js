ef444dc3d67ba91775ff7a675c26d694d31a4e55
lingyun zhao <zhaolingyun1010@gmail.com>
2024-09-05 16:08:27 +0100

    Correct the host for external data icon link

--- app/src/main/javascript/bundles/gene-search/src/ExperimentCard.js
+++ app/src/main/javascript/bundles/gene-search/src/ExperimentCard.js
@@ -54,7 +54,7 @@ class ExperimentCard extends React.Component {
            {experimentDescription}
             <SmallIconDiv>
               {showIcon &&
-                <a href={`${url}/help.html?section=external-data`}>
+                <a href={`${window.location.host}/help.html?section=external-data`}>
                   <ExperimentIconDiv background={`indianred`} color={`white`} data-toggle={`tooltip`}
                                      data-placement={`bottom`} title={`Externally analysed data`}
                   >E</ExperimentIconDiv>

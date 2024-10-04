d3150980d1711417dfe41e5de666dc102346cf47
upendrakumbham <upendra.kumbham@gmail.com>
2024-10-04 17:28:56 +0100

    [Bugfix] Merge pull request #475
    * Add Base64 decoder to decode celltype which is received by the frontend
    
    * Add 'UTF-8' standard format instead of default charset format
    
    * Fix broken tests after adding Base64 decoder for the celltype
    
    * Extract into methods to reuse code and improve code
    
    * Remove variable and add inline code
    
    * Remove variable and use inline code
    
    * Remove Intellij generated @Notnull annotations
    
    * Remove celltype null check
    
    * Format code
    
    * Bump latest version of the scxa-cell-type-wheel-heatmap component

--- app/src/main/java/uk/ac/ebi/atlas/search/metadata/JsonMultiexperimentCellTypeMarkerGenesController.java
+++ app/src/main/java/uk/ac/ebi/atlas/search/metadata/JsonMultiexperimentCellTypeMarkerGenesController.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.atlas.search.metadata;
 
 import com.google.common.collect.ImmutableSet;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,9 @@ import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 import uk.ac.ebi.atlas.experimentpage.markergenes.HighchartsHeatmapAdapter;
 
 import java.net.URLDecoder;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Collection;
 
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
@@ -28,7 +31,7 @@ public class JsonMultiexperimentCellTypeMarkerGenesController extends JsonExcept
     }
 
     @GetMapping(value = "/json/cell-type-marker-genes/{cellType}",
-                produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getCellTypeMarkerGenes(
             @PathVariable String cellType,
             @RequestParam(name = "experiment-accessions", required = false) Collection<String> experimentAccessions) {
@@ -36,9 +39,16 @@ public class JsonMultiexperimentCellTypeMarkerGenesController extends JsonExcept
                 highchartsHeatmapAdapter.getMarkerGeneHeatmapDataSortedLexicographically(
                         experimentAccessions == null ?
                                 multiexperimentCellTypeMarkerGenesService.getCellTypeMarkerGeneProfile(
-                                        URLDecoder.decode(cellType, StandardCharsets.UTF_8)) :
+                                        getDecodedCellType(cellType)) :
                                 multiexperimentCellTypeMarkerGenesService.getCellTypeMarkerGeneProfile(
                                         ImmutableSet.copyOf(experimentAccessions),
-                                        URLDecoder.decode(cellType, StandardCharsets.UTF_8))));
+                                        getDecodedCellType(cellType))));
+    }
+
+    private static String getDecodedCellType(String cellType) {
+        if (cellType == null || cellType.trim().isEmpty()) {
+            throw new IllegalArgumentException("Input cellType cannot be null or empty");
+        }
+        return new String(Base64.getDecoder().decode(cellType), StandardCharsets.UTF_8);
     }
 }

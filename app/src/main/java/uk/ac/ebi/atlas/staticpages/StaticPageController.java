7f4e151cd864387a3827718e15ddb90410667431
Karoly Erdos <karoly@ebi.ac.uk>
2025-04-25 15:11:33 +0100

    Refactor static pages to use Thymeleaf templates.
    Replaced JSP-based static pages with Thymeleaf templates for better maintainability and modernized templating. Added new Thymeleaf templates for "release notes" and "help" pages, incorporating iframe content loading and dynamic section handling. Removed outdated JSP and Tiles configurations.

--- app/src/main/java/uk/ac/ebi/atlas/staticpages/StaticPageController.java
+++ app/src/main/java/uk/ac/ebi/atlas/staticpages/StaticPageController.java
@@ -3,14 +3,15 @@ package uk.ac.ebi.atlas.staticpages;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.support.ServletContextResourceLoader;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 import uk.ac.ebi.atlas.controllers.ResourceNotFoundException;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 
 @Profile("!cli")
 @Controller
@@ -22,25 +23,26 @@ public class StaticPageController extends HtmlExceptionHandlingController {
     }
 
     @RequestMapping("/{pageName}.html")
-    public String getStaticPage(HttpServletRequest request, @PathVariable String pageName) {
-        String path = String.format("/resources/html/%s.html", pageName);
-        request.setAttribute("contentResource", fetchResource(path, request.getRequestURI()));
-        return "static";
+    public String getStaticPage(@PathVariable String pageName, Model model) {
+        checkPageExists(String.format("classpath:/templates/thymeleaf/views/%s.html", pageName), pageName);
+        model.addAttribute("title", pageName);
+        return pageName;
     }
 
-//    @RequestMapping("/help/{pageName}.html")
-//    public String getHelpPage(HttpServletRequest request, @PathVariable String pageName) {
-//        String path = String.format("/resources/html/help/%s.html", pageName);
-//        request.setAttribute("contentResource", fetchResource(path, request.getRequestURI()));
-//        return "static";
-//    }
+    @RequestMapping("/help.html")
+    public String getHelpPage(@RequestParam String section, Model model) {
+        var pageName = "help";
+        checkPageExists(String.format("classpath:/templates/thymeleaf/views/%s.html", pageName), pageName);
+        model.addAttribute("section", section);
+        model.addAttribute("title", "Help");
+        return pageName;
+    }
 
-    private Resource fetchResource(String path, String requestUrl) {
+    private void checkPageExists(String path, String pageName) {
         Resource resource = servletContextResourceLoader.getResource(path);
         if (!resource.exists() || !resource.isReadable()) {
-            throw new ResourceNotFoundException("Resource " + requestUrl + " does not exist");
+            throw new ResourceNotFoundException("Resource " + pageName + " does not exist");
         }
-        return resource;
     }
 
 }

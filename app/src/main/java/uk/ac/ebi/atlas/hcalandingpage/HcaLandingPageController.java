a1cfa6864d0d9763d9020d0575fef5eb7273d5fa
Karoly Erdos <karoly@ebi.ac.uk>
2025-05-15 15:47:34 +0100

    Fix HCA landing page template and controller.
    Updated Thymeleaf template to correct fragment reference syntax. Adjusted controller to declare `ExperimentSearchService` as `final` and added a missing slash to the API mapping URL for better consistency and code clarity.

--- app/src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaLandingPageController.java
+++ app/src/main/java/uk/ac/ebi/atlas/hcalandingpage/HcaLandingPageController.java
@@ -12,13 +12,13 @@ import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
 public class HcaLandingPageController {
-    private ExperimentSearchService experimentSearchService;
+    private final ExperimentSearchService experimentSearchService;
 
     public HcaLandingPageController(ExperimentSearchService experimentSearchService) {
         this.experimentSearchService = experimentSearchService;
     }
 
-    @GetMapping(value = "json/hca/experiments/count", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/json/hca/experiments/count", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getHCAAndHomoSapiensExperiments() {
         HashMap<String, Integer> HCAAndHomoSapiensExperimentsCount = new HashMap<>();
         HCAAndHomoSapiensExperimentsCount.put("hcaExperimentsCount", experimentSearchService.searchPublicExperimentsByAccession(HCA_ACCESSION_PATTERN).size());

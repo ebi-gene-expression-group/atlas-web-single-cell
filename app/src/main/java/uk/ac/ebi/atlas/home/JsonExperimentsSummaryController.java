374c0051913b628aeeee34a6aa478db27c910b59
Karoly Erdos <karoly@ebi.ac.uk>
2025-09-10 11:14:25 +0100

    Add an HCA experiments collection to the featured experiments
    Also update the UI components with new versions and remove unused SVG files

--- app/src/main/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryController.java
+++ app/src/main/java/uk/ac/ebi/atlas/home/JsonExperimentsSummaryController.java
@@ -62,6 +62,11 @@ public class JsonExperimentsSummaryController extends JsonExceptionHandlingContr
                         IMAGE,
                         getExperimentsSummaryImageUrl("discovair"),
                         getExperimentCollectionLink("discovAIR", "discovAIR"),
-                        ImmutableList.of()));
+                        ImmutableList.of()),
+                CardModel.create(
+                    IMAGE,
+                    getExperimentsSummaryImageUrl("hca"),
+                    getExperimentCollectionLink("Human Cell Atlas", "Human Cell Atlas"),
+                    ImmutableList.of()));
     }
 }
